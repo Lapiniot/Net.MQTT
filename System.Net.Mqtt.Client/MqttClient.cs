503a165d8d47134975989f7a2766ae74728a98dc
Lapiniot <aossss@gmail.com>
2018-08-28 22:25:37 +0300

    MqttClient ConnectAsync implementation draft

--- System.Net.Mqtt.Client/MqttClient.cs
+++ System.Net.Mqtt.Client/MqttClient.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
+using System.Net.Mqtt.Client.Exceptions;
+using System.Net.Mqtt.Messages;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Net.Mqtt.Client
@@ -23,18 +26,39 @@ namespace System.Net.Mqtt.Client
 
         public IPEndPoint Endpoint { get; }
 
-        protected override async Task OnConnectAsync(MqttConnectionOptions options)
+        protected override async Task OnConnectAsync(MqttConnectionOptions options, CancellationToken cancellationToken)
         {
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             await socket.ConnectAsync(Endpoint);
-            TryConnect();
-        }
+            var message = new ConnectMessage(ClientId)
+            {
+                KeepAlive = 60,
+                CleanSession = options.CleanSession,
+                UserName = options.UserName,
+                Password = options.Password,
+                LastWillTopic = options.LastWillTopic,
+                LastWillMessage = options.LastWillMessage,
+                LastWillQoS = options.LastWillQoS,
+                LastWillRetain = options.LastWillRetain
+            };
 
-        private void TryConnect()
-        {
-            Span<byte> buffer = stackalloc byte[100];
-            buffer[0] = (byte)PacketType.Connect;
-            socket.Send(buffer);
+            await socket.SendAsync(message.GetBytes(), SocketFlags.None, cancellationToken).ConfigureAwait(false);
+
+            var buffer = new byte[4];
+            var count = await socket.ReceiveAsync(buffer, SocketFlags.None, cancellationToken).ConfigureAwait(false);
+            if(count != 4 || buffer[0] != (byte)PacketType.ConnAck || buffer[1] != 2)
+            {
+                throw new InvalidOperationException("Invalid CONNECT response. Valid CONNACK packet expected.");
+            }
+            switch(buffer[3])
+            {
+                case 0: return;
+                case 1: throw new MqttInvalidProtocolVersionException();
+                case 2: throw new MqttInvalidIdentifierException();
+                case 3: throw new MqttServerUnavailableException();
+                case 4: throw new MqttInvalidUserCredentialsException();
+                case 5: throw new MqttNotAuthorizedException();
+            }
         }
 
         protected override Task OnCloseAsync()
