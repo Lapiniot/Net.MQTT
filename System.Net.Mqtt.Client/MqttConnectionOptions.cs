798bfad4f69c8153ea2874a55797280770f42810
Lapiniot <aossss@gmail.com>
2018-08-28 22:24:16 +0300

    MqttConnectionOptions implemented with basic properties

--- System.Net.Mqtt.Client/MqttConnectionOptions.cs
+++ System.Net.Mqtt.Client/MqttConnectionOptions.cs
@@ -2,5 +2,12 @@ namespace System.Net.Mqtt.Client
 {
     public class MqttConnectionOptions
     {
+        public bool CleanSession { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string LastWillTopic { get; set; }
+        public string LastWillMessage { get; set; }
+        public QoSLevel LastWillQoS { get; set; }
+        public bool LastWillRetain { get; set; }
     }
 }
