6591cde1b0d4cae2f386ba0e070a518c0a512605
Lapiniot <aossss@gmail.com>
2018-08-29 18:53:07 +0300

    MqttMessage properties have been made non-virtual

--- System.Net.Mqtt/MqttMessage.cs
+++ System.Net.Mqtt/MqttMessage.cs
@@ -2,11 +2,11 @@ namespace System.Net.Mqtt
 {
     public abstract class MqttMessage
     {
-        public virtual QoSLevel QoSLevel { get; set; }
+        public QoSLevel QoSLevel { get; set; }
 
-        public virtual bool Duplicate { get; set; }
+        public bool Duplicate { get; set; }
 
-        public virtual bool Retain { get; set; }
+        public bool Retain { get; set; }
 
         public abstract Memory<byte> GetBytes();
     }
