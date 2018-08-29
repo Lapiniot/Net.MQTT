ab13b7a0f46f0e0b3f700d577f1cf8adc1148178
Lapiniot <aossss@gmail.com>
2018-08-29 12:29:38 +0300

    System.Net.Mqtt.Tests project introduced

---
+++ System.Net.Mqtt.Tests/MqttHelpers_EncodeLengthBytes_Should.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Net.Mqtt.MqttHelpers;
+
+namespace System.Net.Mqtt.Tests
+{
+    [TestClass]
+    public class MqttHelpers_EncodeLengthBytes_Should
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Throw_IndexOutOfRangeException_IfInsufficientBufferSizeProvided()
+        {
+            Span<byte> actualBytes = new byte[1];
+            EncodeLengthBytes(2097151, actualBytes);
+        }
+
+        [TestMethod]
+        public void Encode_1_Byte_0_GivenValueOf0()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(0, actualBytes);
+            Assert.AreEqual(1, actualCount);
+            Assert.AreEqual(0, actualBytes[0]);
+            Assert.AreEqual(0, actualBytes[1]);
+            Assert.AreEqual(0, actualBytes[2]);
+            Assert.AreEqual(0, actualBytes[3]);
+        }
+
+        [TestMethod]
+        public void Encode_1_Byte_127_GivenValueOf127()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(127, actualBytes);
+            Assert.AreEqual(1, actualCount);
+            Assert.AreEqual(127, actualBytes[0]);
+            Assert.AreEqual(0, actualBytes[1]);
+            Assert.AreEqual(0, actualBytes[2]);
+            Assert.AreEqual(0, actualBytes[3]);
+        }
+
+        [TestMethod]
+        public void Encode_2_Bytes_128_1_GivenValueOf128()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(128, actualBytes);
+            Assert.AreEqual(2, actualCount);
+            Assert.AreEqual(128, actualBytes[0]);
+            Assert.AreEqual(1, actualBytes[1]);
+            Assert.AreEqual(0, actualBytes[2]);
+            Assert.AreEqual(0, actualBytes[3]);
+        }
+
+        [TestMethod]
+        public void Encode_2_Bytes_255_127_GivenValueOf16383()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(16383, actualBytes);
+            Assert.AreEqual(2, actualCount);
+            Assert.AreEqual(255, actualBytes[0]);
+            Assert.AreEqual(127, actualBytes[1]);
+            Assert.AreEqual(0, actualBytes[2]);
+            Assert.AreEqual(0, actualBytes[3]);
+        }
+
+        [TestMethod]
+        public void Encode_3_Bytes_128_128_1_GivenValueOf16384()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(16384, actualBytes);
+            Assert.AreEqual(3, actualCount);
+            Assert.AreEqual(128, actualBytes[0]);
+            Assert.AreEqual(128, actualBytes[1]);
+            Assert.AreEqual(1, actualBytes[2]);
+            Assert.AreEqual(0, actualBytes[3]);
+        }
+
+        [TestMethod]
+        public void Encode_3_Bytes_255_255_127_GivenValueOf2097151()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(2097151, actualBytes);
+            Assert.AreEqual(3, actualCount);
+            Assert.AreEqual(255, actualBytes[0]);
+            Assert.AreEqual(255, actualBytes[1]);
+            Assert.AreEqual(127, actualBytes[2]);
+            Assert.AreEqual(0, actualBytes[3]);
+        }
+
+        [TestMethod]
+        public void Encode_4_Bytes_128_128_128_1_GivenValueOf2097152()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(2097152, actualBytes);
+            Assert.AreEqual(4, actualCount);
+            Assert.AreEqual(128, actualBytes[0]);
+            Assert.AreEqual(128, actualBytes[1]);
+            Assert.AreEqual(128, actualBytes[2]);
+            Assert.AreEqual(1, actualBytes[3]);
+        }
+
+        [TestMethod]
+        public void Encode_4_Bytes_255_255_255_127_GivenValueOf268435455()
+        {
+            Span<byte> actualBytes = new byte[4];
+            var actualCount = EncodeLengthBytes(268435455, actualBytes);
+            Assert.AreEqual(4, actualCount);
+            Assert.AreEqual(255, actualBytes[0]);
+            Assert.AreEqual(255, actualBytes[1]);
+            Assert.AreEqual(255, actualBytes[2]);
+            Assert.AreEqual(127, actualBytes[3]);
+        }
+    }
+}
---
+++ System.Net.Mqtt.Tests/MqttHelpers_EncodeString_Should.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Net.Mqtt.Tests
+{
+    [TestClass]
+    public class MqttHelpers_EncodeString_Should
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Throw_ArgumentOutOfRangeException_IfInsufficientBufferSizeProvided()
+        {
+            Span<byte> actualBytes = new byte[1];
+            MqttHelpers.EncodeString("abc", actualBytes);
+        }
+
+        [TestMethod]
+        public void Encode_AsValidUtf8Bytes_BigEndianWordSizePrefixed_GivenAsciiString()
+        {
+            Span<byte> actualBytes = new byte[5];
+            var actualSize = MqttHelpers.EncodeString("abc", actualBytes);
+            Assert.AreEqual(5, actualSize);
+            Assert.AreEqual(0, actualBytes[0]);
+            Assert.AreEqual(3, actualBytes[1]);
+            Assert.AreEqual(97, actualBytes[2]);
+            Assert.AreEqual(98, actualBytes[3]);
+            Assert.AreEqual(99, actualBytes[4]);
+        }
+
+        [TestMethod]
+        public void Encode_AsValidUtf8Bytes_BigEndianWordSizePrefixed_GivenUnicodeString()
+        {
+            Span<byte> actualBytes = new byte[12];
+            var actualSize = MqttHelpers.EncodeString("abc-абв", actualBytes);
+            Assert.AreEqual(12, actualSize);
+            Assert.AreEqual(0, actualBytes[0]);
+            Assert.AreEqual(10, actualBytes[1]);
+            Assert.AreEqual(97, actualBytes[2]);
+            Assert.AreEqual(98, actualBytes[3]);
+            Assert.AreEqual(99, actualBytes[4]);
+            Assert.AreEqual(45, actualBytes[5]);
+            Assert.AreEqual(208, actualBytes[6]);
+            Assert.AreEqual(176, actualBytes[7]);
+            Assert.AreEqual(208, actualBytes[8]);
+            Assert.AreEqual(177, actualBytes[9]);
+            Assert.AreEqual(208, actualBytes[10]);
+            Assert.AreEqual(178, actualBytes[11]);
+        }
+    }
+}
