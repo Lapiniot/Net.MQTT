5034b72e08fcddeac4f4e0b7163b75f66916ccfa
Lapiniot <aossss@gmail.com>
2018-08-31 12:33:33 +0300

    Socket reader pipeline logic moved to base abstract NetworkStreamParser

--- System.Net.Mqtt.Client/MqttClient.cs
+++ System.Net.Mqtt.Client/MqttClient.cs
@@ -1,32 +1,23 @@
 ﻿using System.Buffers;
 using System.Collections.Concurrent;
 using System.IO;
-using System.IO.Pipelines;
 using System.Net.Mqtt.Messages;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using static System.Net.Mqtt.PacketFlags;
 using static System.Net.Mqtt.QoSLevel;
-using static System.Net.Sockets.AddressFamily;
-using static System.Net.Sockets.ProtocolType;
 using static System.Net.Sockets.SocketFlags;
 
 namespace System.Net.Mqtt.Client
 {
-    public class MqttClient : AsyncConnectedObject<MqttConnectionOptions>
+    public class MqttClient : NetworkStreamParser<MqttConnectionOptions>
     {
-        private Pipe pipe;
-        private Task processor;
-        private CancellationTokenSource processorCts;
-        private Socket socket;
-        private volatile int currentId = 0;
+        private readonly UInt16IdentityPool idPool = new UInt16IdentityPool();
+        private readonly ConcurrentDictionary<ushort, MqttMessage> map = new ConcurrentDictionary<ushort, MqttMessage>();
 
-        ConcurrentDictionary<ushort, MqttMessage> map = new ConcurrentDictionary<ushort, MqttMessage>();
-
-        public MqttClient(IPEndPoint endpoint, string clientId)
+        public MqttClient(IPEndPoint endpoint, string clientId) : base(endpoint)
         {
-            Endpoint = endpoint;
             ClientId = clientId;
         }
 
@@ -37,13 +28,25 @@ namespace System.Net.Mqtt.Client
 
         public string ClientId { get; }
 
-        public IPEndPoint Endpoint { get; }
-
-        protected override async Task OnConnectAsync(MqttConnectionOptions options, CancellationToken cancellationToken)
+        public async Task PublishAsync(string topic, Memory<byte> payload,
+            QoSLevel qosLevel = AtMostOnce, bool retain = false, CancellationToken token = default)
         {
-            socket = new Socket(InterNetwork, SocketType.Stream, Tcp);
-            await socket.ConnectAsync(Endpoint);
+            CheckConnected();
 
+            var message = new PublishMessage(topic, payload) {QoSLevel = qosLevel, Retain = retain};
+
+            if(qosLevel != AtMostOnce) message.PacketId = idPool.Rent();
+
+            await Socket.SendAsync(message.GetBytes(), None, token).ConfigureAwait(false);
+
+            if(qosLevel == AtLeastOnce)
+            {
+                map.AddOrUpdate(message.PacketId, message, (id, m) => m);
+            }
+        }
+
+        private async Task MqttConnectAsync(MqttConnectionOptions options, CancellationToken cancellationToken)
+        {
             var message = new ConnectMessage(ClientId)
             {
                 KeepAlive = options.KeepAlive,
@@ -56,77 +59,21 @@ namespace System.Net.Mqtt.Client
                 WillRetain = options.LastWillRetain
             };
 
-            await socket.SendAsync(message.GetBytes(), None, cancellationToken).ConfigureAwait(false);
+            await Socket.SendAsync(message.GetBytes(), None, cancellationToken).ConfigureAwait(false);
 
             var buffer = new byte[4];
-            var received = await socket.ReceiveAsync(buffer, None, cancellationToken).ConfigureAwait(false);
+            var received = await Socket.ReceiveAsync(buffer, None, cancellationToken).ConfigureAwait(false);
             new ConnAckMessage(buffer.AsSpan(0, received)).EnsureSuccessStatusCode();
-
-            pipe = new Pipe(new PipeOptions(minimumSegmentSize: 512));
-            processorCts = new CancellationTokenSource();
-            var token = processorCts.Token;
-            processor = Task.WhenAll(
-                Task.Run(() => StartNetworkReader(pipe.Writer, token), token),
-                Task.Run(() => StartParser(pipe.Reader, token), token));
         }
 
-        private async Task StartNetworkReader(PipeWriter writer, CancellationToken token)
+        private async Task MqttDisconnectAsync()
         {
-            try
-            {
-                while(!token.IsCancellationRequested)
-                {
-                    var buffer = writer.GetMemory();
-
-                    var received = await socket.ReceiveAsync(buffer, None, token).ConfigureAwait(false);
-
-                    if(received == 0) break;
-
-                    writer.Advance(received);
-
-                    var result = await writer.FlushAsync(token).ConfigureAwait(false);
-
-                    if(result.IsCompleted) break;
-                }
-            }
-            catch(OperationCanceledException)
-            {
-            }
-            finally
-            {
-                writer.Complete();
-            }
+            await Socket.SendAsync(new byte[] {(byte)PacketType.Disconnect, 0}, None, default).ConfigureAwait(false);
         }
 
-        private async Task StartParser(PipeReader reader, CancellationToken token)
-        {
-            try
-            {
-                while(!token.IsCancellationRequested)
-                {
-                    var result = await reader.ReadAsync(token).ConfigureAwait(false);
+        #region Overrides of NetworkStreamParser<MqttConnectionOptions>
 
-                    var buffer = result.Buffer;
-
-                    if(buffer.IsEmpty) continue;
-
-                    ParseBuffer(buffer, out var consumed);
-
-                    reader.AdvanceTo(buffer.GetPosition(consumed));
-
-                    if(result.IsCompleted) break;
-                }
-            }
-            catch(OperationCanceledException)
-            {
-            }
-            finally
-            {
-                reader.Complete();
-            }
-        }
-
-        private void ParseBuffer(in ReadOnlySequence<byte> buffer, out int consumed)
+        protected override void ParseBuffer(in ReadOnlySequence<byte> buffer, out int consumed)
         {
             consumed = 0;
 
@@ -165,37 +112,26 @@ namespace System.Net.Mqtt.Client
             }
         }
 
+        protected override async Task OnConnectAsync(MqttConnectionOptions options, CancellationToken cancellationToken)
+        {
+            await base.OnConnectAsync(options, cancellationToken).ConfigureAwait(false);
+            await MqttConnectAsync(options, cancellationToken).ConfigureAwait(false);
+        }
+
         protected override async Task OnCloseAsync()
         {
-            processorCts.Cancel();
-            await processor.ConfigureAwait(false);
-
-            await socket.SendAsync(new byte[] { (byte)PacketType.Disconnect, 0 }, None, default);
-
-            socket.Disconnect(false);
-            socket.Shutdown(SocketShutdown.Both);
-            socket.Close();
-        }
-
-        public async Task PublishAsync(string topic, Memory<byte> payload,
-            QoSLevel qosLevel = AtMostOnce, bool retain = false, CancellationToken token = default)
-        {
-            CheckConnected();
-
-            var message = new PublishMessage(topic, payload) { QoSLevel = qosLevel, Retain = retain };
-            if(qosLevel != QoSLevel.AtMostOnce) message.PacketId = GenerateId();
-
-            await socket.SendAsync(message.GetBytes(), None, token).ConfigureAwait(false);
-            
-            if(qosLevel == QoSLevel.AtLeastOnce)
+            try
             {
-                map.AddOrUpdate(message.PacketId, message, (Id, m1) => m1);
+                await MqttDisconnectAsync().ConfigureAwait(false);
             }
-        }
+            catch
+            {
+                // ignored
+            }
 
-        private ushort GenerateId()
-        {
-            return (ushort)Interlocked.Increment(ref currentId);
+            await base.OnCloseAsync().ConfigureAwait(false);
         }
     }
+
+    #endregion
 }
