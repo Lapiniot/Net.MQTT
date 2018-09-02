80cfd94b43a895ee5a7fbe6f2441f8d49d045df3
Lapiniot <aossss@gmail.com>
2018-09-02 17:20:32 +0300

    SubscribeMessage: 1. Fixed bug with wrong header value 2. PacketId != 0 validation in constructor

--- System.Net.Mqtt/Messages/SubscribeMessage.cs
+++ System.Net.Mqtt/Messages/SubscribeMessage.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using static System.Net.Mqtt.MqttHelpers;
 using static System.Text.Encoding;
 
 namespace System.Net.Mqtt.Messages
@@ -8,6 +9,8 @@ namespace System.Net.Mqtt.Messages
     {
         public SubscribeMessage(ushort packetId) : base(packetId)
         {
+            if(packetId == 0) throw new ArgumentException($"{nameof(packetId)} cannot have value of 0");
+
             Topics = new List<(string, QoSLevel)>();
         }
 
@@ -22,20 +25,20 @@ namespace System.Net.Mqtt.Messages
         {
             var payloadLength = Topics.Sum(t => UTF8.GetByteCount(t.topic) + 3);
             var remainingLength = payloadLength + 2;
-            var buffer = new byte[1 + MqttHelpers.GetLengthByteCount(remainingLength) + remainingLength];
+            var buffer = new byte[1 + GetLengthByteCount(remainingLength) + remainingLength];
             Span<byte> m = buffer;
 
-            m[0] = (byte)((int)PacketType & 0b0010);
+            m[0] = (byte)((int)PacketType | 0b0010);
             m = m.Slice(1);
 
-            m = m.Slice(MqttHelpers.EncodeLengthBytes(remainingLength, m));
+            m = m.Slice(EncodeLengthBytes(remainingLength, m));
             m[0] = (byte)(PacketId >> 8);
             m[1] = (byte)(PacketId & 0x00ff);
             m = m.Slice(2);
 
             foreach(var t in Topics)
             {
-                m = m.Slice(MqttHelpers.EncodeString(t.topic, m));
+                m = m.Slice(EncodeString(t.topic, m));
                 m[0] = (byte)t.qosLevel;
                 m = m.Slice(1);
             }
