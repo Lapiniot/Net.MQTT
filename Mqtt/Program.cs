1ddb7c4e3a1a80c10f122d317b6ee0b25fd0666e
Lapiniot <aossss@gmail.com>
2018-10-05 16:31:41 +0300

    Mqtt test console app added

---
+++ Mqtt/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Mqtt.Client;
+using System.Net.Transports;
+using System.Policies;
+using System.Threading.Tasks;
+using static System.Net.Mqtt.QoSLevel;
+using static System.Text.Encoding;
+
+namespace Mqtt
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            //var transport = new TcpSocketsTransport("broker.hivemq.com", 1883);
+            var transport = new WebSocketsTransport(new Uri("ws://broker.hivemq.com:8000/mqtt"), "mqttv3.1", "mqtt");
+
+            var reconnectPolicy = new RetryPolicyBuilder()
+                //.WithTimeout(FromSeconds(15))
+                .WithThreshold(100)
+                .WithDelay(TimeSpan.FromSeconds(5))
+                .WithJitter(100, 1000)
+                .Build();
+
+            using (var client = new MqttClient(transport, "uzm41kyk-ibc",
+                new MqttConnectionOptions {KeepAlive = 10, CleanSession = false}, true, reconnectPolicy))
+            {
+                client.Connected += (sender, args) =>
+                    Console.WriteLine($"Connected ({(args.CleanSession ? "clean session" : "persistent session")}).");
+
+                client.MessageReceived += (sender, m) =>
+                {
+                    var v = UTF8.GetString(m.Payload.Span);
+                    Console.WriteLine(m.Topic + " : " + v);
+                };
+
+                client.Disconnected += (sender, args) =>
+                    Console.WriteLine(args.Aborted ? "Connection aborted." : "Disconnected.");
+
+                await client.ConnectAsync().ConfigureAwait(false);
+                await client.SubscribeAsync(new[] {("lapin/test-topic/messages", ExactlyOnce)}).ConfigureAwait(true);
+
+                await client.PublishAsync("lapin/test-topic/msg", UTF8.GetBytes("my test packet 1"), ExactlyOnce).ConfigureAwait(false);
+                await client.PublishAsync("lapin/test-topic/msg", UTF8.GetBytes("my test packet 2"), AtLeastOnce).ConfigureAwait(false);
+                await client.PublishAsync("lapin/test-topic/msg", UTF8.GetBytes("my test packet 3")).ConfigureAwait(false);
+
+                Console.WriteLine("Press any key to disconnect from MQTT server...");
+                Console.ReadKey();
+
+                await client.UnsubscribeAsync(new[] {"lapin/test-topic/messages"}).ConfigureAwait(false);
+                await client.DisconnectAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
