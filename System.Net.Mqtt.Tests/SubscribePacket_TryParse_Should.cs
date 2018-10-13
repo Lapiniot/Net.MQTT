211d31898a6857e922761b08f1719ad9e3905340
Lapiniot <aossss@gmail.com>
2018-10-13 16:39:39 +0300

    SubscribePacket.TryParse

---
+++ System.Net.Mqtt.Tests/SubscribePacket_TryParse_Should.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Mqtt.Packets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Net.Mqtt.QoSLevel;
+
+namespace System.Net.Mqtt.Tests
+{
+    [TestClass]
+    public class SubscribePacket_TryParse_Should
+    {
+        private readonly byte[] sample =
+        {
+            0x82, 0x1a, 0x00, 0x02, 0x00, 0x05, 0x61, 0x2f,
+            0x62, 0x2f, 0x63, 0x02, 0x00, 0x05, 0x64, 0x2f,
+            0x65, 0x2f, 0x66, 0x01, 0x00, 0x05, 0x67, 0x2f,
+            0x68, 0x2f, 0x69, 0x00
+        };
+
+        private readonly SubscribePacket samplePacket = new SubscribePacket(2)
+        {
+            Topics =
+            {
+                ("a/b/c", ExactlyOnce),
+                ("d/e/f", AtLeastOnce),
+                ("g/h/i", AtMostOnce)
+            }
+        };
+
+        [TestMethod]
+        public void ReturnTrue_PacketNotNull_GivenValidSample()
+        {
+            var actual = SubscribePacket.TryParse(sample, out var packet);
+
+            Assert.IsTrue(actual);
+            Assert.IsNotNull(packet);
+            Assert.AreEqual(3, packet.Topics.Count);
+            Assert.AreEqual("a/b/c", packet.Topics[0]);
+            Assert.AreEqual("d/e/f", packet.Topics[1]);
+            Assert.AreEqual("g/h/i", packet.Topics[2]);
+        }
+    }
+}
--- System.Net.Mqtt/Packets/SubscribePacket.cs
+++ System.Net.Mqtt/Packets/SubscribePacket.cs
@@ -2,11 +2,15 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static System.Buffers.Binary.BinaryPrimitives;
+using static System.Net.Mqtt.MqttHelpers;
 
 namespace System.Net.Mqtt.Packets
 {
     public class SubscribePacket : MqttPacketWithId
     {
+        protected internal const int HeaderValue = (byte)PacketType.Subscribe | 0b0010;
+
         public SubscribePacket(ushort id, params (string, QoSLevel)[] topics) : base(id)
         {
             if(id == 0) throw new ArgumentException($"{nameof(id)} cannot have value of 0");
@@ -16,26 +20,26 @@ namespace System.Net.Mqtt.Packets
 
         public List<(string topic, QoSLevel qosLevel)> Topics { get; }
 
-        protected override byte Header { get; } = (byte)PacketType.Subscribe;
+        protected override byte Header { get; } = HeaderValue;
 
         public override Memory<byte> GetBytes()
         {
             var payloadLength = Topics.Sum(t => Encoding.UTF8.GetByteCount(t.topic) + 3);
             var remainingLength = payloadLength + 2;
-            var buffer = new byte[1 + MqttHelpers.GetLengthByteCount(remainingLength) + remainingLength];
+            var buffer = new byte[1 + GetLengthByteCount(remainingLength) + remainingLength];
             Span<byte> m = buffer;
 
-            m[0] = (byte)(Header | 0b0010);
+            m[0] = HeaderValue;
             m = m.Slice(1);
 
-            m = m.Slice(MqttHelpers.EncodeLengthBytes(remainingLength, m));
+            m = m.Slice(EncodeLengthBytes(remainingLength, m));
             m[0] = (byte)(Id >> 8);
             m[1] = (byte)(Id & 0x00ff);
             m = m.Slice(2);
 
             foreach(var t in Topics)
             {
-                m = m.Slice(MqttHelpers.EncodeString(t.topic, m));
+                m = m.Slice(EncodeString(t.topic, m));
                 m[0] = (byte)t.qosLevel;
                 m = m.Slice(1);
             }
@@ -53,9 +57,19 @@ namespace System.Net.Mqtt.Packets
             throw new NotImplementedException();
         }
 
-        private static bool TryParse(in ReadOnlySpan<byte> source, out SubscribePacket packet)
+        public static bool TryParse(ReadOnlySpan<byte> source, out SubscribePacket packet)
         {
-            throw new NotImplementedException();
+            if(TryParseHeader(source, out var flags, out var length, out var offset) &&
+               (flags & HeaderValue) == HeaderValue && offset + length <= source.Length)
+            {
+                source = source.Slice(offset);
+                packet = new SubscribePacket(ReadUInt16BigEndian(source));
+                source = source.Slice(2);
+                return true;
+            }
+
+            packet = null;
+            return false;
         }
     }
 }
