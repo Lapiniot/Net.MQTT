e81ad7f9598cb78484c484a95c310ca25e55fb11
Lapiniot <aossss@gmail.com>
2018-10-21 02:44:16 +0300

    MqttTopicsHelpers.Matches + unit-tests

---
+++ System.Net.Mqtt.Tests/MqttTopicHelpersTests/MqttTopicHelpers_Matches_Should.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Net.Mqtt.MqttTopicHelpers;
+
+namespace System.Net.Mqtt.MqttTopicHelpersTests
+{
+    [TestClass]
+    public class MqttTopicHelpers_Matches_Should
+    {
+        [TestMethod]
+        public void ReturnFalse_GivenNullTopic()
+        {
+            var actual = Matches(null, "");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_GivenEmptyTopic()
+        {
+            var actual = Matches(string.Empty, "");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenMultiLevelWildCardOnly()
+        {
+            var actual = Matches("a/b/c", "#");
+            Assert.IsTrue(actual);
+
+            actual = Matches("a", "#");
+            Assert.IsTrue(actual);
+
+            actual = Matches("/", "#");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenMultiLevelWildCard()
+        {
+            var actual = Matches("a/b/c", "a/b/#");
+            Assert.IsTrue(actual);
+
+            actual = Matches("a/b/c", "a/#");
+            Assert.IsTrue(actual);
+
+            actual = Matches("a/b/c/", "a/b/c/#");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenStrictMatch()
+        {
+            var actual = Matches("a/b/c", "a/b/c");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_GivenStrictFilter_AndPartialMatch()
+        {
+            var actual = Matches("a/b/c/d", "a/b/c");
+            Assert.IsFalse(actual);
+
+            actual = Matches("a/b/c", "a/b/c/d");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_GivenStrictFilter_NotMatchingTopic()
+        {
+            var actual = Matches("c/d/e", "a/b/c");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenOneLevelWildcard()
+        {
+            var actual = Matches("aaaa/b/c", "+/b/c");
+            Assert.IsTrue(actual);
+
+            actual = Matches("a/bbbb/c", "a/+/c");
+            Assert.IsTrue(actual);
+
+            actual = Matches("a/b/cccc", "a/b/+");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_GivenOneLevelWildcard_AndMoreLevelsTopic()
+        {
+            var actual = Matches("aa/aa/b/c", "+/b/c");
+            Assert.IsFalse(actual);
+
+            actual = Matches("a/bb/bb/c", "a/+/c");
+            Assert.IsFalse(actual);
+
+            actual = Matches("a/b/cc/cc", "a/b/+");
+            Assert.IsFalse(actual);
+
+            actual = Matches("a/b/cccc/", "a/b/+");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenOneLevelWildcard_AndTrailingSlash()
+        {
+            var actual = Matches("a/b/", "a/b/+");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenOneLevelWildcard_AndLeadingSlash()
+        {
+            var actual = Matches("/b/c", "+/b/c");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenTwoOneLevelWildcards_AndSlashTopic()
+        {
+            var actual = Matches("/", "+/+");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_GivenOneLevelWildcard_AndSlashTopic()
+        {
+            var actual = Matches("/", "+");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenMultiLevelWildcard_AndSlashTopic()
+        {
+            var actual = Matches("/", "#");
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_GivenOneLevelWildcard_AndExtraTrailingSlash()
+        {
+            var actual = Matches("a/b/cccc/", "a/b/+");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_GivenOneLevelWildcard_AndExtraLeadingSlash()
+        {
+            var actual = Matches("/aaaa/b/c", "+/b/c");
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_GivenMultipleOneLevelWildcardFilter()
+        {
+            var actual = Matches("a/bbbb/c/dddd/e", "a/+/c/+/e");
+            Assert.IsTrue(actual);
+
+            actual = Matches("aaaa/b/cccc/d/eeee/f/gggg", "+/b/+/d/+/f/+");
+            Assert.IsTrue(actual);
+
+            actual = Matches("aaaa/bbbb/cccc", "+/+/+");
+            Assert.IsTrue(actual);
+        }
+    }
+}
--- System.Net.Mqtt/MqttTopicHelpers.cs
+++ System.Net.Mqtt/MqttTopicHelpers.cs
@@ -27,5 +27,50 @@
 
             return true;
         }
+
+        public static bool Matches(string topic, string filter)
+        {
+            if(string.IsNullOrEmpty(topic)) return false;
+
+            if(filter == "#") return true;
+
+            ReadOnlySpan<char> t = topic;
+            ReadOnlySpan<char> f = filter;
+
+            var topicLength = topic.Length;
+
+            var topicIndex = 0;
+
+            for(var index = 0; index < filter.Length; index++)
+            {
+                var current = f[index];
+
+                if(topicIndex < topicLength)
+                {
+                    if(current != t[topicIndex])
+                    {
+                        if(current == '+')
+                        {
+                            // Scan and skip topic characters until level separator occurence
+                            while(topicIndex < topicLength && t[topicIndex] != '/') topicIndex++;
+                            continue;
+                        }
+
+                        return current == '#';
+                    }
+
+                    topicIndex++;
+                }
+                else
+                {
+                    // Edge case: we ran out of characters in the topic sequence.
+                    // Return true only for proper topic filter level wildcard specified.
+                    return current == '#' || current == '+' && t[topicLength - 1] == '/';
+                }
+            }
+
+            // return true only if topic character sequence has been completely scanned
+            return topicIndex == topicLength;
+        }
     }
 }
