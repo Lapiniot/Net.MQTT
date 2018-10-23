89b0f28b5e5273b2c4379773f6fc7718e845eae2
Lapiniot <aossss@gmail.com>
2018-10-24 01:35:57 +0300

    MqttPacketWithId.TryParseGeneric added

--- System.Net.Mqtt/MqttPacketWithId.cs
+++ System.Net.Mqtt/MqttPacketWithId.cs
@@ -1,4 +1,7 @@
-﻿using static System.Net.Mqtt.Properties.Resources;
+﻿using System.Buffers;
+using System.Net.Mqtt.Buffers;
+using static System.Net.Mqtt.PacketFlags;
+using static System.Net.Mqtt.Properties.Resources;
 
 namespace System.Net.Mqtt
 {
@@ -17,6 +20,44 @@ namespace System.Net.Mqtt
         public override Memory<byte> GetBytes()
         {
             return new byte[] {Header, 2, (byte)(Id >> 8), (byte)(Id & 0x00ff)};
+        }
+
+        protected internal static bool TryParseGeneric(in ReadOnlySequence<byte> source, PacketType type, out ushort id)
+        {
+            id = 0;
+
+            if(source.First.Length >= 4)
+            {
+                return TryParseGeneric(source.First.Span, type, out id);
+            }
+
+            var se = new SequenceEnumerator<byte>(source);
+
+            if(!se.MoveNext() || (se.Current & TypeMask) != (byte)type) return false;
+
+            if(!se.MoveNext() || se.Current != 2) return false;
+
+            if(!se.MoveNext()) return false;
+
+            var high = se.Current << 8;
+
+            if(!se.MoveNext()) return false;
+
+            id = (ushort)(high | se.Current);
+
+            return true;
+        }
+
+        protected internal static bool TryParseGeneric(in ReadOnlySpan<byte> source, PacketType type, out ushort id)
+        {
+            if(source.Length < 4 || (source[0] & TypeMask) != (byte)type || source[1] != 2)
+            {
+                id = 0;
+                return false;
+            }
+
+            id = (ushort)((source[2] << 8) | source[3]);
+            return true;
         }
     }
 }
