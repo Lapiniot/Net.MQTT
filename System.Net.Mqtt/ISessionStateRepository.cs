587cd2d56fbfee99541a8fa37bebab15ee8efa99
Lapiniot <aossss@gmail.com>
2019-02-20 19:05:24 +0200

    ISessionStateRepository moved to common System.Net.Mqtt

--- System.Net.Mqtt/ISessionStateRepository.cs
+++ System.Net.Mqtt/ISessionStateRepository.cs
@@ -1,6 +1,6 @@
-﻿namespace System.Net.Mqtt.Server
+﻿namespace System.Net.Mqtt
 {
-    public interface ISessionStateRepository<out T> where T : SessionState
+    public interface ISessionStateRepository<out T>
     {
         T GetOrCreate(string clientId, bool cleanSession);
         void Remove(string clientId);
