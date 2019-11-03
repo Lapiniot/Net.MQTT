53b5c04a6c06ef80c7d51b3f0608db8248bf0da8
Lapiniot <aossss@gmail.com>
2019-11-03 17:56:41 +0200

    RunAsync for host will be used

--- Mqtt.Server/Program.cs
+++ Mqtt.Server/Program.cs
@@ -11,25 +11,26 @@ namespace Mqtt.Server
 
     internal class Program
     {
-        private static void Main(string[] args)
+        private static Task Main(string[] args)
         {
-             Host.CreateDefaultBuilder()
-                .ConfigureAppConfiguration((ctx, cb) => cb
-                    .AddJsonFile("appsettings.json", false)
-                    .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", true))
-                .ConfigureHostConfiguration(cb => cb
-                    .AddEnvironmentVariables("MQTT_")
-                    .AddCommandLine(args)
-                    .AddJsonFile("settings.mqtt.json", true))
-                .ConfigureMqttService(o => { })
-                .ConfigureLogging(lb => lb
-                    .SetMinimumLevel(Trace)
-                    .AddConsole()
-                    .AddDebug())
-                .UseWindowsService()
-                .UseSystemd()
-                .Build()
-                .Run();
+            return Host.CreateDefaultBuilder()
+               .ConfigureAppConfiguration((ctx, cb) => cb
+                   .AddJsonFile("appsettings.json", false)
+                   .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", true))
+               .ConfigureHostConfiguration(cb => cb
+                   .AddEnvironmentVariables("MQTT_")
+                   .AddCommandLine(args)
+                   .AddJsonFile("settings.mqtt.json", true))
+               .ConfigureMqttService(o => { })
+               .ConfigureLogging(lb => lb
+                   .SetMinimumLevel(Trace)
+                   .AddConsole()
+                   .AddDebug())
+               .UseWindowsService()
+               .UseSystemd()
+               //.UseConsoleLifetime()
+               .Build()
+               .RunAsync();
         }
     }
 }
