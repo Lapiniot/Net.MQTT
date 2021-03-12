95e0bb566c0966f618ed4017de0c24462ac20fb5
Lapiniot <aossss@gmail.com>
2021-03-12 21:58:44 +0200

    net5.0 targeting

--- Mqtt.Server/Program.cs
+++ Mqtt.Server/Program.cs
@@ -1,27 +1,18 @@
 ﻿using System.Net.Mqtt.Server.Hosting;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Mqtt.Server;
 
-namespace Mqtt.Server
-{
-    internal class Program
-    {
-        private static Task Main(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureHostConfiguration(b => b.AddEnvironmentVariables("MQTT_"))
-                .ConfigureWebHost(b => b
-                    .ConfigureAppConfiguration((ctx, cb) => cb.AddEnvironmentVariables("MQTT_KESTREL_"))
-                    .UseStartup<Startup>()
-                    .UseKestrel((hbc, o) => o.Configure(hbc.Configuration.GetSection("Kestrel"))))
-                .ConfigureMqttService()
-                .UseMqttService()
-                .UseWindowsService()
-                .UseSystemd()
-                .Build()
-                .RunAsync();
-        }
-    }
-}
+await Host.CreateDefaultBuilder(args)
+    .ConfigureHostConfiguration(b => b.AddEnvironmentVariables("MQTT_"))
+    .ConfigureWebHost(b => b
+        .ConfigureAppConfiguration((ctx, cb) => cb.AddEnvironmentVariables("MQTT_KESTREL_"))
+        .UseStartup<Startup>()
+        .UseKestrel((hbc, o) => o.Configure(hbc.Configuration.GetSection("Kestrel"))))
+    .ConfigureMqttService()
+    .UseMqttService()
+    .UseWindowsService()
+    .UseSystemd()
+    .Build()
+    .RunAsync();
