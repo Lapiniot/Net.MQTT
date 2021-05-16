a8376d2ea2b9f74229cdb667d02d2b7f5894295e
Lapiniot <aossss@gmail.com>
2021-05-16 12:13:58 +0300

    redundant code removed from record-like Message type

--- System.Net.Mqtt.Server/Message.cs
+++ System.Net.Mqtt.Server/Message.cs
@@ -1,39 +1,12 @@
 ﻿namespace System.Net.Mqtt.Server
 {
-    public class Message
+    public record Message(string Topic, in Memory<byte> Payload, byte QoSLevel, bool Retain)
     {
-        public Message(string topic, in Memory<byte> payload, byte qoSLevel, bool retain)
-        {
-            Topic = topic;
-            Payload = payload;
-            QoSLevel = qoSLevel;
-            Retain = retain;
-        }
-
-        public string Topic { get; }
-        public Memory<byte> Payload { get; }
-        public byte QoSLevel { get; }
-        public bool Retain { get; }
-
-        public void Deconstruct(out string topic, out Memory<byte> payload, out byte qoSLevel, out bool retain)
-        {
-            topic = Topic;
-            payload = Payload;
-            qoSLevel = QoSLevel;
-            retain = Retain;
-        }
-
         public void Deconstruct(out string topic, out Memory<byte> payload, out byte qoSLevel)
         {
             topic = Topic;
             payload = Payload;
             qoSLevel = QoSLevel;
-        }
-
-        public void Deconstruct(out string topic, out Memory<byte> payload)
-        {
-            topic = Topic;
-            payload = Payload;
         }
     }
 }
