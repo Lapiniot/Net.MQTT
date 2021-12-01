8d2cbf46f27c874ec393e327f4409bfee6a095d6
Lapiniot <aossss@gmail.com>
2021-12-01 11:12:51 +0200

    minor code simplification in HttpServerWebSocketAdapter

--- System.Net.Mqtt.Server.AspNetCore.Hosting/HttpServerWebSocketAdapter.cs
+++ System.Net.Mqtt.Server.AspNetCore.Hosting/HttpServerWebSocketAdapter.cs
@@ -1,8 +1,6 @@
 ﻿using System.Net.Connections;
 using System.Net.WebSockets;
 using System.Threading.Channels;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.Options;
 
 using static System.Threading.Channels.BoundedChannelFullMode;
@@ -13,14 +11,10 @@ public class HttpServerWebSocketAdapter : IAsyncEnumerable<INetworkConnection>, 
 {
     private readonly ChannelReader<HttpServerWebSocketConnection> reader;
     private readonly ChannelWriter<HttpServerWebSocketConnection> writer;
-    private readonly ICollection<string> addresses;
 
-    public HttpServerWebSocketAdapter(IOptions<WebSocketListenerOptions> options, IServer server)
+    public HttpServerWebSocketAdapter(IOptions<WebSocketListenerOptions> options)
     {
         ArgumentNullException.ThrowIfNull(options);
-        ArgumentNullException.ThrowIfNull(server);
-
-        addresses = server.Features.Get<IServerAddressesFeature>().Addresses;
 
         var channel = Channel.CreateBounded<HttpServerWebSocketConnection>(
             new BoundedChannelOptions(options.Value.QueueCapacity) { SingleReader = true, SingleWriter = false, FullMode = Wait });
@@ -66,11 +60,7 @@ public class HttpServerWebSocketAdapter : IAsyncEnumerable<INetworkConnection>, 
             {
                 break;
             }
-
-            if(connection is not null)
-            {
-                yield return connection;
-            }
+            yield return connection;
         }
     }
 
@@ -78,6 +68,6 @@ public class HttpServerWebSocketAdapter : IAsyncEnumerable<INetworkConnection>, 
 
     public override string ToString()
     {
-        return $"{nameof(HttpServerWebSocketAdapter)} {{{string.Join(", ", addresses)}}}";
+        return $"{nameof(HttpServerWebSocketAdapter)}";
     }
 }
