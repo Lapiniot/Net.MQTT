f78ba08e4899a80de3429a9a76762efcd3eb8554
Lapiniot <aossss@gmail.com>
2021-12-22 17:42:45 +0200

    simple ObjectPool draft

---
+++ System.Net.Mqtt.Server/ObjectPool.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+
+namespace System.Net.Mqtt.Server;
+
+public sealed class ObjectPool<T> where T : new()
+{
+    private readonly ConcurrentQueue<T> store = new();
+    private readonly int maxInstances;
+    private int instances;
+
+    public ObjectPool(int maxInstances = 50)
+    {
+        this.maxInstances = maxInstances;
+    }
+
+    private static readonly Lazy<ObjectPool<T>> instance = new(() => new ObjectPool<T>(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+#pragma warning disable CA1000
+    public static ObjectPool<T> Shared => instance.Value;
+#pragma warning restore CA100
+
+    public T Rent()
+    {
+        return store.TryDequeue(out T instance) ? instance : new T();
+    }
+
+    public void Return(T instance)
+    {
+        store.Enqueue(instance);
+    }
+}
