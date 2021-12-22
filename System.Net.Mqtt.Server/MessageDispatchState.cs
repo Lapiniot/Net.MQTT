05a7c43a4f6e55957039832bd49c8ef4fcc67061
Lapiniot <aossss@gmail.com>
2021-12-22 17:43:39 +0200

    MessageDispatchState state wrapper

---
+++ System.Net.Mqtt.Server/MessageDispatchState.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace System.Net.Mqtt.Server;
+
+internal struct MessageDispatchState
+{
+    private Message message;
+    private ILogger logger;
+    private Func<MqttServerSessionState, CancellationToken, ValueTask> dispatchFunc;
+    private static readonly Action<ILogger, string, string, int, byte, bool, Exception> logMessage =
+        LoggerMessage.Define<string, string, int, byte, bool>(LogLevel.Debug, 17,
+            "Outgoing message for '{ClientId}': Topic = '{Topic}', Size = {Size}, QoS = {Qos}, Retain = {Retain}");
+
+    public MessageDispatchState(Message message, ILogger logger)
+    {
+        this.message = message;
+        this.logger = logger;
+        dispatchFunc = null;
+    }
+
+    public Message Message { get => message; set => message = value; }
+
+    public ILogger Logger { get => logger; set => logger = value; }
+
+    public Func<MqttServerSessionState, CancellationToken, ValueTask> Dispatcher =>
+        dispatchFunc ??= new Func<MqttServerSessionState, CancellationToken, ValueTask>(DispatchAsync);
+
+    private ValueTask DispatchAsync(MqttServerSessionState sessionState, CancellationToken cancellationToken)
+    {
+        var (topic, payload, qos, _) = message;
+
+        if(!sessionState.TopicMatches(topic, out var level))
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        var adjustedQoS = Math.Min(qos, level);
+
+        var msg = qos == adjustedQoS ? message : message with { QoSLevel = adjustedQoS };
+
+        logMessage(logger, sessionState.ClientId, topic, payload.Length, adjustedQoS, false, null);
+
+        return sessionState.EnqueueAsync(msg, cancellationToken);
+    }
+}
