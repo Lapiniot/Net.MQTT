72b70386479b5937b578520dfe6f15861fd4414e
Lapiniot <aossss@gmail.com>
2022-01-17 19:09:24 +0200

    ObjectPool optimized

--- System.Net.Mqtt.Server/ObjectPool.cs
+++ System.Net.Mqtt.Server/ObjectPool.cs
@@ -2,30 +2,33 @@
 
 namespace System.Net.Mqtt.Server;
 
-public sealed class ObjectPool<T> where T : new()
+public sealed class ObjectPool<T> where T : class, new()
 {
-    private readonly ConcurrentQueue<T> store = new();
-    private readonly int maxInstances;
-    private int instances;
+    private readonly ConcurrentBag<T> store = new();
 
-    public ObjectPool(int maxInstances = 50)
+    public ObjectPool()
     {
-        this.maxInstances = maxInstances;
     }
 
-    private static readonly Lazy<ObjectPool<T>> instance = new(() => new ObjectPool<T>(), LazyThreadSafetyMode.ExecutionAndPublication);
-
 #pragma warning disable CA1000
-    public static ObjectPool<T> Shared => instance.Value;
-#pragma warning restore CA100
+
+    public static ObjectPool<T> Shared => InstanceHolder.instance;
+
+#pragma warning restore CA1000
 
     public T Rent()
     {
-        return store.TryDequeue(out T instance) ? instance : new T();
+        return store.TryTake(out var value) ? value : new T();
     }
 
     public void Return(T instance)
     {
-        store.Enqueue(instance);
+        store.Add(instance);
+    }
+
+    private class InstanceHolder
+    {
+        static InstanceHolder() { }
+        internal static readonly ObjectPool<T> instance = new();
     }
 }
