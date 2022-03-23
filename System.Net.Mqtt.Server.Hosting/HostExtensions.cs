05089ef9491b02f6ffc8d47a5d94175303fc30e8
Lapiniot <aossss@gmail.com>
2022-03-23 11:19:14 +0200

    HostExtensions.WaitForApplicationStartedAsync simplified

--- System.Net.Mqtt.Server.Hosting/HostExtensions.cs
+++ System.Net.Mqtt.Server.Hosting/HostExtensions.cs
@@ -4,14 +4,13 @@ namespace System.Net.Mqtt.Server.Hosting;
 
 public static class HostExtensions
 {
-    public static async Task WaitForApplicationStartedAsync(this IHostApplicationLifetime applicationLifetime, CancellationToken stoppingToken)
+    public static async Task WaitForApplicationStartedAsync(this IHostApplicationLifetime applicationLifetime, CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(applicationLifetime);
         var tcs = new TaskCompletionSource();
         await using (applicationLifetime.ApplicationStarted.Register(static state => ((TaskCompletionSource)state).TrySetResult(), tcs))
-        await using (stoppingToken.Register(static (state, token) => ((TaskCompletionSource)state).TrySetCanceled(token), tcs))
         {
-            await tcs.Task.ConfigureAwait(false);
+            await tcs.Task.WaitAsync(cancellationToken).ConfigureAwait(false);
         }
     }
 }
