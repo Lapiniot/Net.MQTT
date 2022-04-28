a68f2483de4e8a35c17d33e20405d5ff85908fa5
Lapiniot <aossss@gmail.com>
2022-04-28 12:35:47 +0300

    add tool --version switch support

--- Mqtt.Benchmark/Program.cs
+++ Mqtt.Benchmark/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Configuration;
+using System.Reflection;
+using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
@@ -6,6 +8,20 @@ using Mqtt.Benchmark;
 using Mqtt.Benchmark.Configuration;
 
 #pragma warning disable CA1812 // False positive from roslyn analyzer
+
+if (args.Length > 0 && args[0] is "--version" or "-v")
+{
+    Console.OutputEncoding = Encoding.UTF8;
+
+    var assembly = Assembly.GetExecutingAssembly();
+    var description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()!.Description;
+    var version = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()!.Version;
+    var copyright = assembly.GetCustomAttribute<AssemblyCopyrightAttribute>()!.Copyright;
+
+    Console.WriteLine($"{description} v{version} ({copyright})");
+
+    return;
+}
 
 var builder = Host.CreateDefaultBuilder()
     .ConfigureAppConfiguration((_, configuration) => configuration.AddCommandArguments(args))
