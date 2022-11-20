711e9d8721bd94a70dd92a49b293c6f19da35193
Lapiniot <aossss@gmail.com>
2022-11-20 19:41:41 +0200

    Blazor admin dashboard draft template added

--- Mqtt.Server/Program.cs
+++ Mqtt.Server/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Authentication.Certificate;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
 builder.Services.AddWebSocketInterceptor();
 builder.Services.AddHealthChecks().AddMemoryCheck();
 builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
@@ -35,6 +38,20 @@ else if (OperatingSystem.IsWindows())
 }
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.UseAuthentication();
 app.UseWebSockets();
