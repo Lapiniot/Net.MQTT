a86fda1145e86f7975b9fba7b89f15e0717e5229
Lapiniot <aossss@gmail.com>
2022-11-28 12:36:23 +0200

    Changed default admin user to use "mqtt-admin" password

--- Mqtt.Server/Data/Migrations/20221122150458_CreateIdentityData.cs
+++ Mqtt.Server/Data/Migrations/20221122150458_CreateIdentityData.cs
@@ -14,9 +14,10 @@ public partial class CreateIdentityData : Migration
         migrationBuilder.InsertData("AspNetRoles", columns, new[] { "79c881a5-e920-4472-8b68-4984a54a180e", "Admin", "ADMIN", null });
         migrationBuilder.InsertData("AspNetRoles", columns, new[] { "27ce91cf-4f0f-4c80-8d8a-a628a579d5e3", "Client", "CLIENT", null });
 
+        // Create default admin user "admin:mqtt-admin"
         migrationBuilder.InsertData("AspNetUsers",
             new[] { "Id", "UserName", "NormalizedUserName", "Email", "NormalizedEmail", "EmailConfirmed", "PasswordHash", "SecurityStamp", "ConcurrencyStamp", "PhoneNumber", "PhoneNumberConfirmed", "TwoFactorEnabled", "LockoutEnd", "LockoutEnabled", "AccessFailedCount" },
-            new object[] { "013e7f3c-2ea2-4592-a191-9c58db323fcd", "admin", "ADMIN", "", "", true, "AQAAAAIAAYagAAAAEDt0n7z1mjMhw/tyGLa+lpRr7rZ4V1CxOX8W/P1Jkhx+rC4C5it9NczVsmUjAgMAiQ==", "QOJ5ZYOX4VSVUR3AFLWYJ76MEAGE6X5P", "39ff5671-eadf-493c-aed6-289b56899ef5", null, false, false, null, true, 0 });
+            new object[] { "013e7f3c-2ea2-4592-a191-9c58db323fcd", "admin", "ADMIN", "", "", true, "AQAAAAIAAYagAAAAEEQdyDpdd6xTzS+wuQlIDjhQvIraquzo/G4FTTEkGxV8LaVE0VF4h71K4uNSX5vP5g==", "QOJ5ZYOX4VSVUR3AFLWYJ76MEAGE6X5P", "39ff5671-eadf-493c-aed6-289b56899ef5", null, false, false, null, true, 0 });
 
         migrationBuilder.InsertData("AspNetUserRoles", new[] { "UserId", "RoleId" }, new[] { "013e7f3c-2ea2-4592-a191-9c58db323fcd", "79c881a5-e920-4472-8b68-4984a54a180e" });
     }
