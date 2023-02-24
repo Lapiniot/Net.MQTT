02fea2969f093d7df484b5218ffe56632f1e02b6
Lapiniot <aossss@gmail.com>
2023-02-24 20:57:29 +0200

    Benchmarks: disable tiered compilation and tiered PGO for default job in order to test aggressively optimized JIT tier1 output

--- System.Net.Mqtt.Benchmarks/Program.cs
+++ System.Net.Mqtt.Benchmarks/Program.cs
@@ -31,10 +31,13 @@ internal sealed partial class Program
             }
         }
 
-        config.AddJob(Job.Default
+        config.AddJob(Job.Default.WithId("AggressiveJITOptimization")
             .WithEnvironmentVariables(
                 //new EnvironmentVariable("DOTNET_JitDisasm", "TopicMatches"),
-                new EnvironmentVariable("DOTNET_TieredPGO", "0")));
+                new EnvironmentVariable("DOTNET_TieredCompilation", "0"),
+                new EnvironmentVariable("DOTNET_TieredPGO", "0"),
+                new EnvironmentVariable("DOTNET_TC_QuickJitForLoops", "0"),
+                new EnvironmentVariable("DOTNET_ReadyToRun", "1")));
 
         return config;
     }
