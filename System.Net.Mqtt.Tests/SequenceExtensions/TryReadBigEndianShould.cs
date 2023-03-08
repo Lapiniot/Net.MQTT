d0ff4b9993dfe09c877986c478ac164b10f9baa3
Lapiniot <aossss@gmail.com>
2023-03-08 11:13:10 +0200

    SequenceExtensions.TryReadBigEndianShould UTs refactored + some more cases added

--- System.Net.Mqtt.Tests/SequenceExtensions/TryReadBigEndianShould.cs
+++ System.Net.Mqtt.Tests/SequenceExtensions/TryReadBigEndianShould.cs
@@ -6,29 +6,12 @@ namespace System.Net.Mqtt.Tests.SequenceExtensions;
 [TestClass]
 public class TryReadBigEndianShould
 {
-    private readonly ReadOnlySequence<byte> completeSequence;
-    private readonly ReadOnlySequence<byte> emptySequence;
-    private readonly ReadOnlySequence<byte> fragmentedSequence;
-    private readonly ReadOnlySequence<byte> fragmentedSequenceIncomplete;
-    private readonly ReadOnlySequence<byte> fragmentedSequenceInterlaced;
-    private readonly ReadOnlySequence<byte> fragmentedSequenceInterlacedIncomplete;
-    private readonly ReadOnlySequence<byte> incompleteSequence;
-
-    public TryReadBigEndianShould()
-    {
-        completeSequence = new(new byte[] { 0x40, 0xCD });
-        emptySequence = new(Array.Empty<byte>());
-        incompleteSequence = new(new byte[] { 0x40 });
-        fragmentedSequence = SequenceFactory.Create(new byte[] { 0x40 }, new byte[] { 0xFF });
-        fragmentedSequenceInterlaced = SequenceFactory.Create(Array.Empty<byte>(), new byte[] { 0x40 }, Array.Empty<byte>(), Array.Empty<byte>(), new byte[] { 0xFF });
-        fragmentedSequenceIncomplete = SequenceFactory.Create(new byte[] { 0x40 });
-        fragmentedSequenceInterlacedIncomplete = SequenceFactory.Create(Array.Empty<byte>(), new byte[] { 0x40 }, Array.Empty<byte>());
-    }
-
     [TestMethod]
     public void ReturnFalse_GivenEmptySequence()
     {
-        var actual = TryReadBigEndian(in emptySequence, out var value);
+        var sequence = new ReadOnlySequence<byte>(Array.Empty<byte>());
+
+        var actual = TryReadBigEndian(in sequence, out var value);
 
         Assert.IsFalse(actual);
         Assert.AreEqual(0, value);
@@ -37,52 +20,66 @@ public class TryReadBigEndianShould
     [TestMethod]
     public void ReturnFalse_GivenIncompleteSequence()
     {
-        var actual = TryReadBigEndian(in incompleteSequence, out var value);
+        var sequence = new ReadOnlySequence<byte>(new byte[] { 0x40 });
+
+        var actual = TryReadBigEndian(in sequence, out var value);
 
         Assert.IsFalse(actual);
         Assert.AreEqual(0, value);
     }
 
     [TestMethod]
-    public void ReturnFalse_GivenFragmentedIncompleteSequence()
+    public void ReturnFalse_GivenIncompleteFragmentedSequence()
     {
-        var actual = TryReadBigEndian(in fragmentedSequenceIncomplete, out var value);
+        var sequence = SequenceFactory.Create(Array.Empty<byte>(), new byte[] { 0x40 }, Array.Empty<byte>());
+
+        var actual = TryReadBigEndian(in sequence, out var value);
 
         Assert.IsFalse(actual);
         Assert.AreEqual(0, value);
     }
 
     [TestMethod]
-    public void ReturnFalse_GivenFragmentedIncompleteInterlacedSequence()
+    public void ReturnTrue_GivenCompleteSolidSpanSequence()
     {
-        var actual = TryReadBigEndian(in fragmentedSequenceInterlacedIncomplete, out var value);
+        var sequence = new ReadOnlySequence<byte>(new byte[] { 0x40, 0xCD });
 
-        Assert.IsFalse(actual);
-        Assert.AreEqual(0, value);
-    }
-
-    [TestMethod]
-    public void ReturnTrue_GivenCompleteSequence()
-    {
-        var actual = TryReadBigEndian(in completeSequence, out var actualValue);
+        var actual = TryReadBigEndian(in sequence, out var actualValue);
 
         Assert.IsTrue(actual);
         Assert.AreEqual(0x40cd, actualValue);
     }
 
     [TestMethod]
-    public void ReturnTrue_GivenFragmentedSequence()
+    public void ReturnTrue_GivenCompleteSolidSpanInterlacedSequence()
     {
-        var actual = TryReadBigEndian(in fragmentedSequence, out var actualValue);
+        var sequence = SequenceFactory.Create(Array.Empty<byte>(), new byte[] { 0x40, 0xCD }, Array.Empty<byte>());
+
+        var actual = TryReadBigEndian(in sequence, out var actualValue);
+
+        Assert.IsTrue(actual);
+        Assert.AreEqual(0x40cd, actualValue);
+    }
+
+    [TestMethod]
+    public void ReturnTrue_GivenCompleteFragmentedSequence()
+    {
+        var sequence = SequenceFactory.Create(new byte[] { 0x40 }, new byte[] { 0xFF });
+
+        var actual = TryReadBigEndian(in sequence, out var actualValue);
 
         Assert.IsTrue(actual);
         Assert.AreEqual(0x40FF, actualValue);
     }
 
     [TestMethod]
-    public void ReturnTrue_GivenFragmentedInterlacedSequence()
+    public void ReturnTrue_GivenCompleteFragmentedInterlacedSequence()
     {
-        var actual = TryReadBigEndian(in fragmentedSequenceInterlaced, out var actualValue);
+        var sequence = SequenceFactory.Create(
+            Array.Empty<byte>(), new byte[] { 0x40 }, Array.Empty<byte>(),
+            Array.Empty<byte>(), new byte[] { 0xFF });
+
+        var actual = TryReadBigEndian(in sequence, out var actualValue);
 
         Assert.IsTrue(actual);
         Assert.AreEqual(0x40FF, actualValue);
