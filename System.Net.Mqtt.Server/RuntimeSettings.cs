07a03a2128c6584c9a23be0dcc6e4d87ea8b5ed8
Lapiniot <aossss@gmail.com>
2023-03-16 20:13:06 +0200

    MetricsCollectionSupport feature switch introduced

---
+++ System.Net.Mqtt.Server/RuntimeSettings.cs
@@ -0,0 +1,14 @@
+using System.Runtime.CompilerServices;
+
+namespace System.Net.Mqtt.Server;
+
+public static class RuntimeSettings
+{
+    private static readonly bool metricsCollectionSupport = !AppContext.TryGetSwitch("System.Net.Mqtt.Server.MetricsCollectionSupport", out var isEnabled) || isEnabled;
+
+    public static bool MetricsCollectionSupport
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => metricsCollectionSupport;
+    }
+}
