a1e6ef8184821776141829cc09884e6c43a1bcd6
Lapiniot <aossss@gmail.com>
2023-03-28 20:50:43 +0300

    FastIdentityPoolBenchmarks parametrization added

--- System.Net.Mqtt.Benchmarks/IdentityPool/FastIdentityPoolBenchmarks.cs
+++ System.Net.Mqtt.Benchmarks/IdentityPool/FastIdentityPoolBenchmarks.cs
@@ -4,21 +4,31 @@ namespace System.Net.Mqtt.Benchmarks.IdentityPool;
 
 [HideColumns("Error", "StdDev", "RatioSD", "Median")]
 [DisassemblyDiagnoser]
+[MemoryDiagnoser]
 public class FastIdentityPoolBenchmarks
 {
+    public static IEnumerable<ushort> BucketSizes { get; } = new ushort[] { 32, 512 };
+    public static IEnumerable<ushort> Rents { get; } = new ushort[] { 32, 512, 65535 };
+
+    [ParamsSource(nameof(Rents))]
+    public int RentCount { get; set; }
+
+    [ParamsSource(nameof(BucketSizes))]
+    public short BucketSize { get; set; }
+
     [Benchmark(Baseline = true)]
     public void RentParallelV1()
     {
-        var pool = new FastIdentityPoolV1();
+        var pool = new FastIdentityPoolV1(BucketSize);
 
-        Parallel.For(0, 65535, new() { MaxDegreeOfParallelism = 8 }, _ => pool.Rent());
+        Parallel.For(0, RentCount, new() { MaxDegreeOfParallelism = 8 }, _ => pool.Rent());
     }
 
     [Benchmark]
     public void RentParallelNext()
     {
-        var pool = new FastIdentityPool();
+        var pool = new FastIdentityPool(BucketSize);
 
-        Parallel.For(0, 65535, new() { MaxDegreeOfParallelism = 8 }, _ => pool.Rent());
+        Parallel.For(0, RentCount, new() { MaxDegreeOfParallelism = 8 }, _ => pool.Rent());
     }
 }
