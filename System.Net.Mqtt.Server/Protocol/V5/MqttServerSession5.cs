54be50da85fe81178df91126499819967fb79d62
Lapiniot <aossss@gmail.com>
2023-05-08 17:53:52 +0300

    V5: OnPublish chain implementation draft

--- System.Net.Mqtt.Server/Protocol/V5/MqttServerSession5.cs
+++ System.Net.Mqtt.Server/Protocol/V5/MqttServerSession5.cs
@@ -1,5 +1,7 @@
 using System.Net.Mqtt.Packets.V5;
 using static System.Net.Mqtt.PacketType;
+using static System.Net.Mqtt.PacketFlags;
+using static System.Net.Mqtt.Extensions.SequenceExtensions;
 
 namespace System.Net.Mqtt.Server.Protocol.V5;
 
@@ -82,10 +84,10 @@ public class MqttServerSession5 : MqttServerSession
         {
             case CONNECT: break;
             case PUBLISH: OnPublish(header, in reminder); break;
-            case PUBACK: OnPubAck(header, in reminder); break;
-            case PUBREC: OnPubRec(header, in reminder); break;
-            case PUBREL: OnPubRel(header, in reminder); break;
-            case PUBCOMP: OnPubComp(header, in reminder); break;
+            case PUBACK: OnPubAck(in reminder); break;
+            case PUBREC: OnPubRec(in reminder); break;
+            case PUBREL: OnPubRel(in reminder); break;
+            case PUBCOMP: OnPubComp(in reminder); break;
             case SUBSCRIBE: OnSubscribe(header, in reminder); break;
             case UNSUBSCRIBE: OnUnsubscribe(header, in reminder); break;
             case PINGREQ: OnPingReq(); break;
@@ -97,16 +99,86 @@ public class MqttServerSession5 : MqttServerSession
 
     private void OnPublish(byte header, in ReadOnlySequence<byte> reminder)
     {
+        var qos = (header >> 1) & QoSMask;
+        if (!PublishPacket.TryReadPayload(in reminder, qos != 0, (int)reminder.Length, out var id, out var topic, out var payload, out _))
+        {
+            MqttPacketHelpers.ThrowInvalidFormat("PUBLISH");
+        }
+
+        var message = new Message(topic, payload, (byte)qos, (header & Retain) == Retain);
+
+        switch (qos)
+        {
+            case 0:
+                OnMessageReceived(message);
+                break;
+
+            case 1:
+                OnMessageReceived(message);
+                Post(PubAckPacketMask | id);
+                break;
+
+            case 2:
+                // This is to avoid message duplicates for QoS 2
+                if (state!.TryAddQoS2(id))
+                {
+                    OnMessageReceived(message);
+                }
+
+                Post(PubRecPacketMask | id);
+                break;
+
+            default:
+                MqttPacketHelpers.ThrowInvalidFormat("PUBLISH");
+                break;
+        }
     }
 
-    private void OnPubAck(byte header, in ReadOnlySequence<byte> reminder) => throw new NotImplementedException();
-    private void OnPubRec(byte header, in ReadOnlySequence<byte> reminder) => throw new NotImplementedException();
-    private void OnPubRel(byte header, in ReadOnlySequence<byte> reminder) => throw new NotImplementedException();
-    private void OnPubComp(byte header, in ReadOnlySequence<byte> reminder) => throw new NotImplementedException();
+    private void OnPubAck(in ReadOnlySequence<byte> reminder)
+    {
+        if (!TryReadBigEndian(in reminder, out var id))
+        {
+            MqttPacketHelpers.ThrowInvalidFormat("PUBACK");
+        }
+
+        state!.DiscardMessageDeliveryState(id);
+    }
+
+    private void OnPubRec(in ReadOnlySequence<byte> reminder)
+    {
+        if (!TryReadBigEndian(in reminder, out var id))
+        {
+            MqttPacketHelpers.ThrowInvalidFormat("PUBREC");
+        }
+
+        state!.SetMessagePublishAcknowledged(id);
+        Post(PubRelPacketMask | id);
+    }
+
+    private void OnPubRel(in ReadOnlySequence<byte> reminder)
+    {
+        if (!TryReadBigEndian(in reminder, out var id))
+        {
+            MqttPacketHelpers.ThrowInvalidFormat("PUBREL");
+        }
+
+        state!.RemoveQoS2(id);
+        Post(PubCompPacketMask | id);
+    }
+
+    private void OnPubComp(in ReadOnlySequence<byte> reminder)
+    {
+        if (!TryReadBigEndian(in reminder, out var id))
+        {
+            MqttPacketHelpers.ThrowInvalidFormat("PUBCOMP");
+        }
+
+        state!.DiscardMessageDeliveryState(id);
+    }
 
     private void OnSubscribe(byte header, in ReadOnlySequence<byte> reminder)
     {
-        if (header != PacketFlags.SubscribeMask || !SubscribePacket.TryReadPayload(in reminder, (int)reminder.Length, out var id, out _, out _, out var filters))
+        if (header != SubscribeMask || !SubscribePacket.TryReadPayload(in reminder, (int)reminder.Length, out var id, out _, out _, out var filters))
         {
             MqttPacketHelpers.ThrowInvalidFormat("SUBSCRIBE");
             return;
@@ -127,7 +199,7 @@ public class MqttServerSession5 : MqttServerSession
 
     private void OnUnsubscribe(byte header, in ReadOnlySequence<byte> reminder)
     {
-        if (header != PacketFlags.UnsubscribeMask || !UnsubscribePacket.TryReadPayload(in reminder, (int)reminder.Length, out var id, out _, out var filters))
+        if (header != UnsubscribeMask || !UnsubscribePacket.TryReadPayload(in reminder, (int)reminder.Length, out var id, out _, out var filters))
         {
             MqttPacketHelpers.ThrowInvalidFormat("UNSUBSCRIBE");
             return;
@@ -142,7 +214,7 @@ public class MqttServerSession5 : MqttServerSession
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void OnPingReq() => Post(PacketFlags.PingRespPacket);
+    private void OnPingReq() => Post(PingRespPacket);
 
     private void OnDisconnect()
     {
