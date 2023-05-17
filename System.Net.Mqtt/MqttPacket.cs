f72623dddfdc55b877eb33a4aaa51588f8802a83
Lapiniot <aossss@gmail.com>
2023-05-17 11:24:30 +0300

    some new Roslyn analysis warnings eliminated

--- System.Net.Mqtt/MqttPacket.cs
+++ System.Net.Mqtt/MqttPacket.cs
@@ -3,10 +3,10 @@ namespace System.Net.Mqtt;
 public abstract class MqttPacket
 {
     /// <summary>
-    /// Writes binary representation of the packet to the <seealso cref="IBufferWriter{byte}" /> writer.
+    /// Writes binary representation of the packet to the <seealso cref="IBufferWriter{T}" /> writer.
     /// </summary>
     /// <param name="writer">Buffer writer instance.</param>
-    /// <param name="buffer">Underlaying buffer provided by the <paramref name="writer"> for this write operation.</paramref>
+    /// <param name="buffer">Underlaying buffer provided by the <paramref name="writer" /> for this write operation.</param>
     /// <returns>Number of bytes actually written to the <paramref name="buffer" />.</returns>
     public abstract int Write(IBufferWriter<byte> writer, out Span<byte> buffer);
 }
