a05cd5054aea5489c7b57f71d5d0773d2b452196
Lapiniot <aossss@gmail.com>
2023-07-18 22:53:28 +0300

    submodule updated

--- System.Net.Mqtt/MqttPacketWithId.cs
+++ System.Net.Mqtt/MqttPacketWithId.cs
@@ -4,8 +4,7 @@ public abstract class MqttPacketWithId : MqttPacket
 {
     protected MqttPacketWithId(ushort id)
     {
-        if (id == 0)
-            Verify.ThrowValueMustBeInRange(nameof(id), 1, ushort.MaxValue);
+        Verify.ThrowIfNotInRange(id, 1, ushort.MaxValue);
         Id = id;
     }
 
