08802936a562500da2e0d64eb507876f842c76d9
Lapiniot <aossss@gmail.com>
2023-12-01 16:32:32 +0200

    Base32 benchmarks extended

--- System.Net.Mqtt.Benchmarks/Base32/Base32V1.cs
+++ System.Net.Mqtt.Benchmarks/Base32/Base32V1.cs
@@ -2,7 +2,7 @@ namespace System.Net.Mqtt.Benchmarks.Base32;
 
 public static class Base32V1
 {
-    private static readonly char[] Alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'];
+    private static readonly char[] Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7'];
 
     public static string ToBase32String(long value) => string.Create(13, value, WriteBuffer);
 
