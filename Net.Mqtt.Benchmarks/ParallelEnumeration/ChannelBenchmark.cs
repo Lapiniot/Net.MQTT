8fdac65baad60d8db283076acc087bf933a69382
Lapiniot <aossss@gmail.com>
2024-03-05 16:21:45 +0200

    Some new microbenchmarks introduced

---
+++ Net.Mqtt.Benchmarks/ParallelEnumeration/ChannelBenchmark.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Channels;
+
+namespace Net.Mqtt.Benchmarks.ParallelEnumeration;
+
+[MemoryDiagnoser]
+[IterationCount(100)]
+public class ChannelBenchmark
+{
+    private Channel<TaskCompletionSource> data;
+
+    [Params(20, 200, 1000, 10000, 30000)]
+    public int Count { get; set; }
+
+    [IterationSetup]
+    public void Setup()
+    {
+        data = Channel.CreateUnbounded<TaskCompletionSource>();
+        var writer = data.Writer;
+        for (var i = 0; i < Count; i++)
+        {
+            writer.TryWrite(new TaskCompletionSource());
+        }
+
+        writer.Complete();
+    }
+
+    [Benchmark]
+    public void Sequential()
+    {
+        var reader = data.Reader;
+        while (reader.TryRead(out var tcs))
+        {
+            tcs.TrySetCanceled();
+        }
+    }
+
+    [Benchmark]
+    public async Task ReadAllAsync()
+    {
+        await foreach (var item in data.Reader.ReadAllAsync().ConfigureAwait(false))
+        {
+            item.TrySetCanceled();
+        }
+    }
+
+    [Benchmark]
+    public async Task ParallelReadAllAsync()
+    {
+        await Parallel.ForEachAsync(data.Reader.ReadAllAsync(), (cts, ct) =>
+        {
+            cts.TrySetCanceled(ct);
+            return ValueTask.CompletedTask;
+        }).ConfigureAwait(false);
+    }
+}
---
+++ Net.Mqtt.Benchmarks/ParallelEnumeration/DictionaryBenchmark.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+
+namespace Net.Mqtt.Benchmarks.ParallelEnumeration;
+
+[MemoryDiagnoser]
+[IterationCount(100)]
+public class DictionaryBenchmark
+{
+    private ConcurrentDictionary<int, TaskCompletionSource> data = [];
+
+    [Params(20, 200, 1000, 10000, 30000)]
+    public int Count { get; set; }
+
+    [IterationSetup]
+    public void Setup()
+    {
+        data = [];
+        for (var i = 0; i < Count; i++)
+        {
+            data[i] = new TaskCompletionSource();
+        }
+    }
+
+    [Benchmark]
+    public void SequentialForeach()
+    {
+        foreach (var item in data)
+        {
+            item.Value.TrySetCanceled();
+        }
+    }
+
+    [Benchmark]
+    public void ParallelForeach() => Parallel.ForEach(data, static item => item.Value.TrySetCanceled());
+
+    [Benchmark]
+    public void ParallelLinqDefault() => data.AsParallel().AsUnordered().WithMergeOptions(ParallelMergeOptions.Default).ForAll(static item => item.Value.TrySetCanceled());
+
+    [Benchmark]
+    public void ParallelLinqAutoBuffered() => data.AsParallel().AsUnordered().WithMergeOptions(ParallelMergeOptions.AutoBuffered).ForAll(static item => item.Value.TrySetCanceled());
+
+    [Benchmark]
+    public void ParallelLinqNotBuffered() => data.AsParallel().AsUnordered().WithMergeOptions(ParallelMergeOptions.NotBuffered).ForAll(static item => item.Value.TrySetCanceled());
+
+    [Benchmark]
+    public void ParallelLinqFullyBuffered() => data.AsParallel().AsUnordered().WithMergeOptions(ParallelMergeOptions.FullyBuffered).ForAll(static item => item.Value.TrySetCanceled());
+}
