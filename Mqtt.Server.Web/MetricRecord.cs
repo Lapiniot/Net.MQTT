58d0152f5435dd2c4ebc9cc3136d246b80a288ae
Lapiniot <aossss@gmail.com>
2024-07-15 17:39:04 +0300

    Code style warnings (detected by new Roslyn analyzers) have been eliminated Fixes #1

--- Mqtt.Server.Web/MetricRecord.cs
+++ Mqtt.Server.Web/MetricRecord.cs
@@ -9,6 +9,6 @@ public sealed class MetricRecord<T>(string name, string? description, bool enabl
 public abstract class MetricRecord(string name, string? description, bool enabled = true)
 {
     public string? Description { get; } = description;
-    public bool Enabled { get; internal protected set; } = enabled;
+    public bool Enabled { get; protected internal set; } = enabled;
     public string Name { get; } = name;
 }
