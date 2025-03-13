108a0422b66f0c3e3fff2bdf540e26c1aa60a99d
Lapiniot <aossss@gmail.com>
2025-03-13 21:03:16 +0200

    Refactor Net.Mqtt.Client.MqttClientBuilder and remove redundant and obsolete methods Close #60

--- Net.Mqtt.Client/MqttClientBuilder.cs
+++ Net.Mqtt.Client/MqttClientBuilder.cs
@@ -12,27 +12,21 @@ public readonly record struct MqttClientBuilder
     public const int DefaultTcpPort = 1883;
     public const int DefaultSecureTcpPort = 8883;
 
-    public MqttClientBuilder() { }
-
     public int Version { get; private init; } = 3;
-    private Func<TransportConnection>? ConnectionFactory { get; init; }
     private string? ClientId { get; init; }
     private EndPoint? EndPoint { get; init; }
-    private IPAddress? Address { get; init; }
-    private string? HostNameOrAddress { get; init; }
-    private int Port { get; init; }
     private bool UseSsl { get; init; }
     private X509Certificate[]? Certificates { get; init; }
-    private string? MachineName { get; init; }
     private SslProtocols EnabledSslProtocols { get; init; }
-    private bool DisposeConnection { get; init; }
-    private Uri? WsUri { get; init; }
     private Action<ClientWebSocketOptions>? WsConfigureOptions { get; init; }
     private HttpMessageInvoker? WsMessageInvoker { get; init; }
     private int MaxInFlight { get; init; } = ushort.MaxValue >>> 1;
 
-    public MqttClientBuilder WithProtocol(int version) =>
-        Version == version ? this : this with { Version = version is 3 or 4 or 5 ? version : ThrowVersionNotSupported() };
+    public MqttClientBuilder() { }
+
+    public MqttClientBuilder WithProtocol(int version) => Version != version
+        ? this with { Version = version is 3 or 4 or 5 ? version : ThrowVersionNotSupported<int>() }
+        : this;
 
     public MqttClientBuilder WithProtocolV3() => Version == 3 ? this : this with { Version = 3 };
 
@@ -44,19 +38,6 @@ public readonly record struct MqttClientBuilder
 
     public MqttClientBuilder WithMaxInFlight(int maxInFlight) => this with { MaxInFlight = maxInFlight };
 
-    public MqttClientBuilder WithConnection(TransportConnection connection, bool disposeConnection = false) =>
-        this with
-        {
-            EndPoint = null,
-            Address = null,
-            HostNameOrAddress = null,
-            Port = 0,
-            WsUri = null,
-            WsConfigureOptions = null,
-            ConnectionFactory = () => connection,
-            DisposeConnection = disposeConnection
-        };
-
     public MqttClientBuilder WithUri(Uri uri)
     {
         ArgumentNullException.ThrowIfNull(uri);
@@ -64,170 +45,137 @@ public readonly record struct MqttClientBuilder
         return uri switch
         {
             { Scheme: "tcp" or "mqtt", Host: var host, Port: var port } =>
-                WithTcp(host, port > 0 ? port : DefaultTcpPort).WithSsl(false),
+                WithTcp(host, port > 0 ? port : DefaultTcpPort),
             { Scheme: "tcps" or "mqtts", Host: var host, Port: var port } =>
-                WithTcp(host, port > 0 ? port : DefaultSecureTcpPort).WithSsl(true),
+                WithTcpSsl(host, port > 0 ? port : DefaultSecureTcpPort),
             { Scheme: "unix" } or { IsFile: true } =>
                 WithUnixDomain(SocketBuilderExtensions.ResolveUnixDomainSocketPath(uri.LocalPath)),
-            { Scheme: "ws" or "http" } =>
-                WithWebSockets(uri, WsConfigureOptions).WithSsl(false),
-            { Scheme: "wss" or "https" } =>
-                WithWebSockets(uri, WsConfigureOptions).WithSsl(true),
+            { Scheme: "ws" or "http" or "wss" or "https" } =>
+                WithWebSockets(uri),
             _ => ThrowSchemaNotSupported<MqttClientBuilder>()
         };
     }
 
-    public MqttClientBuilder WithWebSockets(Uri uri, Action<ClientWebSocketOptions>? configureOptions = null) =>
-        this with
-        {
-            EndPoint = default,
-            Address = default,
-            HostNameOrAddress = default,
-            Port = default,
-            ConnectionFactory = default,
-            WsUri = uri,
-            WsConfigureOptions = configureOptions
-        };
+    #region HTTP WebSockets
 
-    public MqttClientBuilder WithWebSocketOptions(Action<ClientWebSocketOptions>? configureOptions) =>
-        this with { WsConfigureOptions = configureOptions };
+    public MqttClientBuilder WithWebSockets(Uri uri) => this with
+    {
+        EndPoint = new UriEndPoint(uri),
+        UseSsl = false,
+        EnabledSslProtocols = default
+    };
 
-    public MqttClientBuilder WithWebSocketHttpMessageInvoker(HttpMessageInvoker invoker) =>
-        this with { WsMessageInvoker = invoker };
+    public MqttClientBuilder WithWebSocketOptions(Action<ClientWebSocketOptions>? configureOptions) => this with
+    {
+        WsConfigureOptions = configureOptions
+    };
+
+    public MqttClientBuilder WithWebSocketHttpMessageInvoker(HttpMessageInvoker invoker) => this with
+    {
+        WsMessageInvoker = invoker
+    };
+
+    #endregion
+
+    #region TCP sockets
+
+    private MqttClientBuilder WithTcpEndpoint(EndPoint endPoint) => this with
+    {
+        EndPoint = endPoint,
+        UseSsl = false,
+        EnabledSslProtocols = default,
+        WsConfigureOptions = default,
+        WsMessageInvoker = default
+    };
 
     public MqttClientBuilder WithTcp(IPEndPoint endPoint) =>
-        this with
-        {
-            EndPoint = endPoint,
-            Address = default,
-            HostNameOrAddress = default,
-            Port = default,
-            WsUri = default,
-            WsConfigureOptions = default,
-            ConnectionFactory = default,
-            DisposeConnection = true
-        };
+        WithTcpEndpoint(endPoint);
 
-    public MqttClientBuilder WithTcp(IPAddress address, int port) =>
-        this with
-        {
-            EndPoint = default,
-            Address = address,
-            HostNameOrAddress = default,
-            Port = port,
-            WsUri = default,
-            WsConfigureOptions = default,
-            ConnectionFactory = default,
-            DisposeConnection = true
-        };
+    public MqttClientBuilder WithTcp(IPAddress address, int port = DefaultTcpPort) =>
+        WithTcpEndpoint(new IPEndPoint(address, port));
 
-    public MqttClientBuilder WithTcp(IPAddress address) => WithTcp(address, 0);
+    public MqttClientBuilder WithTcp(string hostNameOrAddress, int port = DefaultTcpPort,
+        AddressFamily addressFamily = AddressFamily.Unspecified) =>
+        WithTcpEndpoint(new DnsEndPoint(hostNameOrAddress, port, addressFamily));
 
-    public MqttClientBuilder WithTcp(string hostNameOrAddress, int port) =>
-        this with
-        {
-            EndPoint = default,
-            Address = default,
-            HostNameOrAddress = hostNameOrAddress,
-            Port = port,
-            WsUri = default,
-            WsConfigureOptions = default,
-            ConnectionFactory = default,
-            DisposeConnection = true
-        };
+    #endregion
 
-    public MqttClientBuilder WithTcp(string hostNameOrAddress) => WithTcp(hostNameOrAddress, 0);
+    #region TCP SSL sockets
 
-    public MqttClientBuilder WithUnixDomain(UnixDomainSocketEndPoint endPoint) =>
-        this with
-        {
-            EndPoint = endPoint,
-            Address = default,
-            HostNameOrAddress = default,
-            Port = default,
-            WsUri = default,
-            WsConfigureOptions = default,
-            ConnectionFactory = default,
-            DisposeConnection = true
-        };
+    private MqttClientBuilder WithTcpSslEndpoint(EndPoint endPoint, SslProtocols enabledSslProtocols) => this with
+    {
+        EndPoint = endPoint,
+        UseSsl = true,
+        EnabledSslProtocols = enabledSslProtocols,
+        WsConfigureOptions = default,
+        WsMessageInvoker = default
+    };
 
-    public MqttClientBuilder WithSsl(bool useSsl = true) =>
-        useSsl == UseSsl ? this :
-        useSsl ? this with
-        {
-            UseSsl = true,
-            ConnectionFactory = default,
-            DisposeConnection = true,
-            EnabledSslProtocols = SslProtocols.None
-        } : this with
-        {
-            UseSsl = false,
-            MachineName = default,
-            EnabledSslProtocols = default
-        };
+    public MqttClientBuilder WithTcpSsl(IPEndPoint endPoint, SslProtocols enabledSslProtocols = SslProtocols.None) =>
+        WithTcpSslEndpoint(endPoint, enabledSslProtocols);
 
-    public MqttClientBuilder WithSsl(string? machineName = null, SslProtocols enabledSslProtocols = SslProtocols.None) =>
-        this with
-        {
-            UseSsl = true,
-            MachineName = machineName,
-            EnabledSslProtocols = enabledSslProtocols,
-            ConnectionFactory = default,
-            DisposeConnection = true
-        };
+    public MqttClientBuilder WithTcpSsl(IPAddress address, int port = DefaultSecureTcpPort,
+        SslProtocols enabledSslProtocols = SslProtocols.None) =>
+        WithTcpSslEndpoint(new IPEndPoint(address, port), enabledSslProtocols);
 
-    public MqttClientBuilder WithClientCertificates(X509Certificate[] certificates) =>
-        this with
-        {
-            UseSsl = true,
-            ConnectionFactory = default,
-            DisposeConnection = true,
-            Certificates = certificates
-        };
+    public MqttClientBuilder WithTcpSsl(string hostNameOrAddress, int port = DefaultSecureTcpPort,
+        AddressFamily addressFamily = AddressFamily.Unspecified, SslProtocols enabledSslProtocols = SslProtocols.None) =>
+        WithTcpSslEndpoint(new DnsEndPoint(hostNameOrAddress, port, addressFamily), enabledSslProtocols);
+
+    public MqttClientBuilder WithClientCertificates(X509Certificate[] certificates) => this with
+    {
+        UseSsl = true,
+        Certificates = certificates
+    };
+
+    #endregion
+
+    #region Unix Domain Sockets
+
+    public MqttClientBuilder WithUnixDomain(UnixDomainSocketEndPoint endPoint) => this with
+    {
+        EndPoint = endPoint,
+        UseSsl = false,
+        EnabledSslProtocols = default,
+        WsConfigureOptions = default,
+        WsMessageInvoker = default
+    };
+
+    #endregion
 
     private TransportConnection BuildConnection()
     {
 #pragma warning disable CA2000 // Dispose objects before losing scope
         return this switch
         {
-            { ConnectionFactory: not null } => ConnectionFactory(),
             { EndPoint: IPEndPoint ipEndPoint, UseSsl: true } =>
-                ClientTcpSslSocketTransportConnection.Create(ipEndPoint, MachineName, EnabledSslProtocols, Certificates),
+                ClientTcpSslSocketTransportConnection.Create(ipEndPoint, null, EnabledSslProtocols, Certificates),
             { EndPoint: IPEndPoint ipEndPoint } =>
                 ClientTcpSocketTransportConnection.Create(ipEndPoint),
             { EndPoint: UnixDomainSocketEndPoint unixDomainEndPoint } =>
                 ClientUnixDomainSocketTransportConnection.Create(unixDomainEndPoint),
-            { Address: { } address, UseSsl: true } =>
-                ClientTcpSslSocketTransportConnection.Create(new(address, Port > 0 ? Port : DefaultSecureTcpPort),
-                    MachineName, EnabledSslProtocols, Certificates),
-            { Address: { } address } =>
-                ClientTcpSocketTransportConnection.Create(new(address, Port > 0 ? Port : DefaultTcpPort)),
-            { HostNameOrAddress: { } host, UseSsl: true } =>
-                ClientTcpSslSocketTransportConnection.Create(host, Port > 0 ? Port : DefaultSecureTcpPort,
-                    machineName: MachineName, enabledSslProtocols: EnabledSslProtocols, clientCertificates: Certificates),
-            { HostNameOrAddress: { } host } =>
-                ClientTcpSocketTransportConnection.Create(host, Port > 0 ? Port : DefaultTcpPort),
-            { WsUri: { } uri } =>
+            { EndPoint: DnsEndPoint dnsEndPoint, UseSsl: true } =>
+                ClientTcpSslSocketTransportConnection.Create(dnsEndPoint, null, EnabledSslProtocols, Certificates),
+            { EndPoint: DnsEndPoint dnsEndPoint } =>
+                ClientTcpSocketTransportConnection.Create(dnsEndPoint),
+            { EndPoint: UriEndPoint { Uri: { } uri } } =>
                 ClientWebSocketTransportConnection.Create(MakeValidWsUri(uri),
                     CreateConfigureCallback(Certificates, WsConfigureOptions), WsMessageInvoker),
             _ => ThrowCannotBuildTransport()
         };
 #pragma warning restore CA2000 // Dispose objects before losing scope
-
     }
 
-    private static Uri MakeValidWsUri(Uri uri)
+    private static Uri MakeValidWsUri(Uri uri) => uri switch
     {
-        return uri switch
-        {
-            { Scheme: "ws" or "wss" } => uri,
-            { Scheme: "http" } => new UriBuilder(uri) { Scheme = "ws" }.Uri,
-            { Scheme: "https" } => new UriBuilder(uri) { Scheme = "wss" }.Uri,
-            _ => ThrowSchemaNotSupported<Uri>()
-        };
-    }
+        { Scheme: "ws" or "wss" } => uri,
+        { Scheme: "http" } => new UriBuilder(uri) { Scheme = "ws" }.Uri,
+        { Scheme: "https" } => new UriBuilder(uri) { Scheme = "wss" }.Uri,
+        _ => ThrowSchemaNotSupported<Uri>()
+    };
 
-    private static Action<ClientWebSocketOptions> CreateConfigureCallback(X509Certificate[]? certificates, Action<ClientWebSocketOptions>? configureOptions)
+    private static Action<ClientWebSocketOptions> CreateConfigureCallback(X509Certificate[]? certificates,
+        Action<ClientWebSocketOptions>? configureOptions)
     {
         return (options) =>
         {
@@ -241,28 +189,25 @@ public readonly record struct MqttClientBuilder
         };
     }
 
-    public MqttClient Build(string? clientId = null)
+    public MqttClient Build(string? clientId = null) => Version switch
     {
-        switch (Version)
-        {
-            case 3: return BuildV3(clientId);
-            case 4: return BuildV4(clientId);
-            case 5: return BuildV5(clientId);
-            default: ThrowVersionNotSupported(); return null;
-        }
-    }
+        3 => BuildV3(clientId),
+        4 => BuildV4(clientId),
+        5 => BuildV5(clientId),
+        _ => ThrowVersionNotSupported<MqttClient>(),
+    };
 
     public MqttClient3 BuildV3(string? clientId = null) =>
-        new(BuildConnection(), DisposeConnection, clientId ?? ClientId ?? Base32.ToBase32String(CorrelationIdGenerator.GetNext()), MaxInFlight);
+        new(BuildConnection(), true, clientId ?? ClientId ?? Base32.ToBase32String(CorrelationIdGenerator.GetNext()), MaxInFlight);
 
     public MqttClient4 BuildV4(string? clientId = null) =>
-        new(BuildConnection(), DisposeConnection, clientId ?? ClientId, MaxInFlight);
+        new(BuildConnection(), true, clientId ?? ClientId, MaxInFlight);
 
     public MqttClient5 BuildV5(string? clientId = null) =>
-        new(BuildConnection(), DisposeConnection, clientId ?? ClientId, MaxInFlight);
+        new(BuildConnection(), true, clientId ?? ClientId, MaxInFlight);
 
     [DoesNotReturn]
-    private static int ThrowVersionNotSupported() =>
+    private static T ThrowVersionNotSupported<T>() =>
         throw new ArgumentException(Strings.UnsupportedProtocolVersion);
 
     [DoesNotReturn]
