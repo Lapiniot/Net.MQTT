a248778892c7a6d8d6429847aa34c4bd159dc096
Lapiniot <aossss@gmail.com>
2025-09-24 12:54:09 +0300

    MSSQL: Add database initialize migrations Fixes #95

---
+++ Mqtt.Server.Identity.SqlServer/Migrations/20250923162027_CreateIdentityData.cs
@@ -0,0 +1,8 @@
+﻿using Mqtt.Server.Identity.Data.Migrations;
+
+namespace Mqtt.Server.Identity.SqlServer.Migrations;
+
+/// <inheritdoc />
+public partial class CreateIdentityData : InitializeIdentityDataMigration
+{
+}
