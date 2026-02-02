c0785c3dbad3df727112597811c9251d35f0bd9a
Lapiniot <aossss@gmail.com>
2026-02-02 21:43:56 +0200

    BUG: MqttServerOptions are not completely setup from app's configuration if MqttServerHostingExtensions.ConfigureMqttServer() was not called explicitely Fixes #129

--- Net.Mqtt.Server.Hosting/MqttServerHostingExtensions.cs
+++ Net.Mqtt.Server.Hosting/MqttServerHostingExtensions.cs
@@ -1,77 +1,140 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using static System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes;
 
+#pragma warning disable CA1034 // Nested types should not be visible
+#pragma warning disable CA1708 // Identifiers should differ by more than case
+
 namespace Net.Mqtt.Server.Hosting;
 
+/// <summary>
+/// Provides extension methods for registering MQTT server services, authentication handlers, and certificate validation policies
+/// with the <see cref="IServiceCollection"/>.
+/// </summary>
+/// <remarks>
+/// These methods add and configure MQTT server-related services, including options validation, authentication, and certificate validation,
+/// to the dependency injection container.
+/// </remarks>
 public static class MqttServerHostingExtensions
 {
-    public static IServiceCollection AddMqttServer(this IServiceCollection services)
+    private const string DefaultSectionName = "MQTT";
+
+    extension(IServiceCollection services)
     {
-        ArgumentNullException.ThrowIfNull(services);
-
-        services.AddOptionsWithValidateOnStart<MqttServerOptions, MqttServerOptionsValidator>();
-        services.TryAddTransient<IMqttServerBuilder, MqttServerBuilder>();
-        services.TryAddSingleton(sp => sp.GetRequiredService<IMqttServerBuilder>().Build());
-        services.AddHostedService<GenericMqttHostService>();
-        return services;
-    }
-
-    [DynamicDependency(All, typeof(MqttServerOptions))]
-    [DynamicDependency(All, typeof(MqttOptions))]
-    [DynamicDependency(All, typeof(MqttOptions5))]
-    [DynamicDependency(All, typeof(MqttEndpoint))]
-    [DynamicDependency(All, typeof(CertificateOptions))]
-    [UnconditionalSuppressMessage("Trimming", "IL3050:Members annotated with 'RequiresUnreferencedCodeAttribute'" +
-        " require dynamic access otherwise can break functionality when trimming application code",
-        Justification = "<Pending>")]
-    [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute'" +
-        " require dynamic access otherwise can break functionality when trimming application code",
-        Justification = "<Pending>")]
-    public static IHostBuilder ConfigureMqttServer(this IHostBuilder hostBuilder,
-        Action<HostBuilderContext, MqttServerOptionsBuilder> configureOptions = null,
-        string configSectionPath = "MQTT")
-    {
-        ArgumentNullException.ThrowIfNull(hostBuilder);
-        ArgumentException.ThrowIfNullOrEmpty(configSectionPath);
-
-        return hostBuilder.ConfigureServices((ctx, services) =>
+        /// <summary>
+        /// Adds required MQTT server services to the <see cref="IServiceCollection"/>
+        /// to run the MQTT server as hosted background service.
+        /// </summary>
+        /// <returns>
+        /// The same <see cref="IServiceCollection"/> so that multiple calls can be chained.
+        /// </returns>
+        [DynamicDependency(All, typeof(MqttServerOptions))]
+        [DynamicDependency(All, typeof(MqttOptions))]
+        [DynamicDependency(All, typeof(MqttOptions5))]
+        [DynamicDependency(All, typeof(MqttEndpoint))]
+        [DynamicDependency(All, typeof(CertificateOptions))]
+        [UnconditionalSuppressMessage("Trimming", "IL3050:Members annotated with 'RequiresUnreferencedCodeAttribute'" +
+            " require dynamic access otherwise can break functionality when trimming application code",
+            Justification = "<Pending>")]
+        [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute'" +
+            " require dynamic access otherwise can break functionality when trimming application code",
+            Justification = "<Pending>")]
+        public IServiceCollection AddMqttServer()
         {
-            services.AddTransient<IConfigureOptions<MqttServerOptions>, MqttServerOptionsSetup>(
-                sp => new(ctx.Configuration.GetSection(configSectionPath)));
-            configureOptions?.Invoke(ctx, new(services.AddOptions<MqttServerOptions>()));
-        });
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddOptionsWithValidateOnStart<MqttServerOptions, MqttServerOptionsValidator>();
+            services.AddTransient<IConfigureOptions<MqttServerOptions>>(
+                sp => new MqttServerOptionsSetup(sp.GetRequiredService<IConfiguration>().GetSection(DefaultSectionName)));
+            services.TryAddTransient<IMqttServerBuilder, MqttServerBuilder>();
+            services.TryAddSingleton(static sp => sp.GetRequiredService<IMqttServerBuilder>().Build());
+            services.AddHostedService<GenericMqttHostService>();
+            return services;
+        }
+
+        /// <summary>
+        /// Registers a custom MQTT authentication handler implementation with the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the authentication handler to register.</typeparam>
+        /// <returns>The same <see cref="IServiceCollection"/> so that multiple calls can be chained.</returns>
+        public IServiceCollection AddMqttAuthentication<[DynamicallyAccessedMembers(All)] T>()
+            where T : class, IMqttAuthenticationHandler
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.TryAddSingleton<IMqttAuthenticationHandler, T>();
+            return services;
+        }
+
+        /// <summary>
+        /// Registers a callback-based MQTT authentication handler with the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="callback">The callback function to handle authentication requests.</param>
+        /// <returns>The same <see cref="IServiceCollection"/> so that multiple calls can be chained.</returns>
+        public IServiceCollection AddMqttAuthentication(Func<string, string, ValueTask<bool>> callback)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(callback);
+
+            services.TryAddSingleton<IMqttAuthenticationHandler>(sp => new CallbackAuthenticationHandler(callback));
+            return services;
+        }
+
+        /// <summary>
+        /// Registers a custom remote certificate validation policy implementation with the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the certificate validation policy to register.</typeparam>
+        /// <returns>The same <see cref="IServiceCollection"/> so that multiple calls can be chained.</returns>
+        public IServiceCollection AddMqttCertificateValidation<[DynamicallyAccessedMembers(All)] T>
+            () where T : class, IRemoteCertificateValidationPolicy
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.TryAddSingleton<IRemoteCertificateValidationPolicy, T>();
+            return services;
+        }
     }
 
-    public static IServiceCollection AddMqttAuthentication<[DynamicallyAccessedMembers(All)] T>
-        (this IServiceCollection services) where T : class, IMqttAuthenticationHandler
+    extension(IHostBuilder hostBuilder)
     {
-        ArgumentNullException.ThrowIfNull(services);
+        /// <summary>
+        /// Configures MQTT server options for the hosted MQTT server instance.
+        /// </summary>
+        /// <param name="configureOptions">An optional action to configure the MQTT server options.</param>
+        /// <param name="configuration">An optional configuration instance to bind MQTT server options from.</param>
+        /// <returns>The same <see cref="IHostBuilder"/> so that multiple calls can be chained.</returns>
+        [DynamicDependency(All, typeof(MqttServerOptions))]
+        [DynamicDependency(All, typeof(MqttOptions))]
+        [DynamicDependency(All, typeof(MqttOptions5))]
+        [DynamicDependency(All, typeof(MqttEndpoint))]
+        [DynamicDependency(All, typeof(CertificateOptions))]
+        [UnconditionalSuppressMessage("Trimming", "IL3050:Members annotated with 'RequiresUnreferencedCodeAttribute'" +
+            " require dynamic access otherwise can break functionality when trimming application code",
+            Justification = "<Pending>")]
+        [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute'" +
+            " require dynamic access otherwise can break functionality when trimming application code",
+            Justification = "<Pending>")]
+        public IHostBuilder ConfigureMqttServer(Action<HostBuilderContext,
+            MqttServerOptionsBuilder> configureOptions = null,
+            IConfiguration configuration = null)
+        {
+            ArgumentNullException.ThrowIfNull(hostBuilder);
 
-        services.TryAddSingleton<IMqttAuthenticationHandler, T>();
-        return services;
-    }
+            return hostBuilder.ConfigureServices((ctx, services) =>
+            {
+                if (configuration is not null)
+                {
+                    services.AddTransient<IConfigureOptions<MqttServerOptions>>(
+                        sp => new MqttServerOptionsSetup(configuration));
+                }
 
-    public static IServiceCollection AddMqttAuthentication(this IServiceCollection services,
-        Func<string, string, ValueTask<bool>> callback)
-    {
-        ArgumentNullException.ThrowIfNull(services);
-        ArgumentNullException.ThrowIfNull(callback);
-
-        services.TryAddSingleton<IMqttAuthenticationHandler>(sp => new CallbackAuthenticationHandler(callback));
-        return services;
-    }
-
-    public static IServiceCollection AddMqttCertificateValidation<[DynamicallyAccessedMembers(All)] T>
-        (this IServiceCollection services) where T : class, IRemoteCertificateValidationPolicy
-    {
-        ArgumentNullException.ThrowIfNull(services);
-
-        services.TryAddSingleton<IRemoteCertificateValidationPolicy, T>();
-        return services;
+                configureOptions?.Invoke(ctx, new(services.AddOptions<MqttServerOptions>()));
+            });
+        }
     }
 
     private sealed class CallbackAuthenticationHandler(Func<string, string, ValueTask<bool>> callback) :
